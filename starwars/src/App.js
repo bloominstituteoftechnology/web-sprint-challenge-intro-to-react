aaa7e691a0c162c9d39f9a045ec73c5212cafb78
Elizabeth Hagag <lizjanegiles@gmail.com>
2020-06-26 19:05:46 -0500

    updates

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,25 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import Character from './components/Character';
 
 const App = () => {
 
-  // const [ StarWarsData, setStarWarsData ] = useState([]);
+  const [ StarWarsData, setStarWarsData ] = useState([]);
 
   useEffect(() => {
     axios
       .get('https://swapi.py4e.com/api/people/')
       .then(res =>{
         console.log("Star Wars Data:",res)
+        setStarWarsData(res.data);
       })
       .catch(err=> {
         console.log('Error:',err)
       })
-  })
+  },[])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      {StarWarsData.map(ppl => {
+        return (
+          <Character
+            key={ppl.id}
+            name={ppl.title}
+            height={ppl.height}
+            mass={ppl.mass}
+            hairColor={ppl.hair_color} />
+        );
+      })}
+
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,15 @@
-// Write your Character component here
+import React from 'react'
+
+function Character({name, height, mass, hairColor, skinColor}) {
+    return (
+        <div>
+            <h2>{name}</h2>
+            <p>Height : {height}</p>
+            <p>Mass : {mass}</p>
+            <p>Hair Color : {hairColor}</p>
+            <p>Skin Color : {skinColor}</p>
+        </div>
+    )
+}
+
+export default Character
