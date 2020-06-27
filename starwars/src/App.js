43496fa17d676b596dee0ba9bec5ec3bf054ec02
Anthony Bratcher <tonybrat74@gmail.com>
2020-06-26 17:13:29 -0700

    axiosrequest

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+const [data, setData] = useState([])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+     axios.get("https://swapi.py4e.com/api/people/")
+
+     .then((res)=>{
+       console.log(res)
+       setData(res.date.results)
+
+     })
+
+     .catch((err)=>{
+       console.log(err)
+
+     })
+
+  }, [])
 
   return (
     <div className="App">
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,2 @@
 // Write your Character component here
+import React, { useState, useEffect} from 'react'
