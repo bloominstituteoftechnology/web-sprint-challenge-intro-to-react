1949a38a470b0b42f3c17ce121241d6609d5e651
ngriffith29 <ngriffith29@student.egcc.edu>
2020-06-28 19:31:33 -0400

    go everything working

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,36 @@
 // Write your Character component here
+import React from "react";
+import '../App.css'
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+} from "reactstrap";
+export default function Character(props) {
+  let card = props.characters.map((e) => {
+    return (
+        <div className="card">
+            <div>
+                <h2>{e.name}</h2>
+                <h2>{Math.round(e.height/12)} LBS</h2>
+            </div>
+
+        </div>
+ 
+        
+        
+        
+        )
+    ;
+  });
+  console.log(props.characters);
+  return (
+    <div>
+      <div>{card}</div>
+    </div>
+  );
+}
