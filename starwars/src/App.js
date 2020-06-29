89823923b8b129fc2c057f55dce80566ece414c4
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-06-28 20:10:01 -0400

    making my axios call to get info

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,6 +9,16 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [character, setCharacter] = useState([]);
+  useEffect(() => {
+    axios
+    .get("https://swapi.dev/api/people/")
+    .then(response => {
+      console.log("this is from the api", response)
+    })
+    .catch(error => console.log("error", error));
+  }, []);
 
   return (
     <div className="App">
