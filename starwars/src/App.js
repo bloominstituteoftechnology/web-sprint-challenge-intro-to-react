a0349964c6f3e29dca2f8f4d5b5467d13082c1a9
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-06-28 20:52:52 -0400

    having problems passing props

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from "axios";
+import Characters from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -15,7 +16,8 @@ const App = () => {
     axios
     .get("https://swapi.dev/api/people/")
     .then(response => {
-      console.log("this is from the api", response)
+      console.log("this is from the api", response.data.results)
+      setCharacter(response.data.results);
     })
     .catch(error => console.log("error", error));
   }, []);
@@ -23,6 +25,14 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {this.props.characters.map(person => {
+        return(
+
+          <Characters person={person}/>
+        )
+      
+
+      })}
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import React from "react";
+import App from "../App";
+
+const Characters = props => {
+    return (
+        <div className="App">         
+        <h3>Name: {props.character.name}</h3>
+        <h3>Gender: {props.character.gender} </h3>
+        <h3>Home: {props.character.homeworld}</h3>
+        <img url={props.character.img_url} />   
+              
+        </div>
+    )
+}
+
+export default Characters;
