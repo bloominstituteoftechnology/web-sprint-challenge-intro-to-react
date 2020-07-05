99de95a5c01fa43e53665008f1dcd902471d7235
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-07-05 17:40:20 -0400

    added styled component to charachter component

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,18 +1,28 @@
 // Write your Character component here
 import React from "react";
 import App from "../App";
+import styled from "styled-components";
+
+
+
+const WrapperDiv = styled.div`
+font-family: cursive;
+text-align: center;
+border: 1px solid black;
+
+`;
 
 
 const Characters = ({name, gender, homeworld, img_url}) => {
     return (
-        <div className="App">
+        <WrapperDiv className="App">
                 <div>
                  <h3>Name: {name}</h3>
                  <h3>Gender: {gender} </h3>
                  <h3>Home: {homeworld}</h3>
                  <img url={img_url} />
                 </div>             
-        </div>
+        </WrapperDiv>
     )
 }
 export default Characters;
