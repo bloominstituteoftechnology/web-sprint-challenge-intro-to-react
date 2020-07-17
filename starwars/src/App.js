00f4f0e47e4e5224ddb5c6c17771746bd46116ea
kevin lam <kevin@kevins-MacBook-Pro-3.local>
2020-07-17 08:42:04 -0700

    initial commit

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+
+import Character from './components/Character'
+
 
 const App = () => {
+  const [characters, setCharacters] = useState([])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then((res) => {
+      setCharacters(res.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  })
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
