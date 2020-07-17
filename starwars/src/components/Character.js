bee55440980f430978ec2a9556f3ba1d056dfe68
Ashley <ashley.bergsma42@gmail.com>
2020-07-17 10:42:26 -0700

    It's ALIVE!

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,10 +1,23 @@
 // Write your Character component here
 import React from 'react'; 
-import style from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 import CharacterCard from './CharacterCard'
 
-const StyledDetails = style.div`
+const kf = keyframes`
+    0%{
+        transform: scale(0);
+    }
+    50%{
+        transform: scale(0.3);
+    }
+    100%{
+        transform: scale(1);
+    }
+`
+
+const StyledDetails = styled.div`
+    animation: ${kf} .5s ease-in-out forwards;
     background-color: white;
     width: 35%;
     margin: 0 auto;
@@ -13,11 +26,13 @@ const StyledDetails = style.div`
     flex-direction: column;
     border-radius: 5%;
     box-shadow: 4px 4px gray;
-
     h3 {
         font-family: 'Rowdies', cursive;
         font-size: 30px;
         text-shadow: 3px 2px gray;
+    }
+    p {
+        font-family: 'Rowdies', cursive;
     }
 `
 
