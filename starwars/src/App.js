d6bb6f43d1f8d935adb1db8eefc4468833842b6b
Ashley <ashley.bergsma42@gmail.com>
2020-07-17 10:57:26 -0700

    zoom zoom spaceship icon

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,8 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import styled from 'styled-components';
 
 import Character from './components/Character'
+
+const StyledApp = styled.div`
+    .welcomeHeader {
+      display: flex;
+      justify-content: center;
+    }
+    h1 {
+      font-size: 50px;
+    }
+    img {
+      height: 10vh;
+    }
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -15,6 +29,7 @@ const App = () => {
   // sync up with, if any.
 
   useEffect(() => {
+
     axios.get('https://swapi.dev/api/people')
     .then(res => {
       // console.log(res.data.results);
@@ -24,11 +39,14 @@ const App = () => {
       console.log('There was an error, message:' + err);
     })
   }, []); 
-console.log(characters)
+// console.log(characters)
 
   return (
-    <div className="App">
+    <StyledApp className="App">
+      <div className="welcomeHeader">
       <h1 className="Header">Eisleygram</h1>
+      <img src="https://cdn2.iconfinder.com/data/icons/space-icostory-black-and-white/64/rocket-space-shuttle-spaceship-512.png" />
+        </div>
         {characters.map(character => {
           return ( 
           <Character key={character.created}
@@ -42,7 +60,7 @@ console.log(characters)
           )
         })}
 
-    </div>
+    </StyledApp>
   );
 }
 
