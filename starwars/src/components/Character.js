33a8034bd68a2bfae37d65ca7a400cea904a7bb7
kevin lam <kevin@kevins-MacBook-Pro-3.local>
2020-07-17 09:30:37 -0700

    animations added

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -8,14 +8,21 @@ width: 90%;
 margin: 0 auto;
 display: flex;
 flex-wrap: wrap;
-justify-content: space-between
+justify-content: space-between;
+div:hover {
+    transition: 0.6s ease-in-out;
+    transform: scale(1.1) rotate(360deg);
+   
+}
 `
+
 const Cards = styled.div`
 border: 2px solid black;
 width: 30%;
 min-width: 200px;
 margin-bottom: 1rem;
-background-color: silver;
+background-color: rgba(50, 0, 200, 0.3);
+color: white;
 `
 
 const Characters = (props) => {
@@ -26,10 +33,11 @@ const Characters = (props) => {
       {
       characters.map((p) => {
         return (
-        <Cards>
-            <h2>Name: {p.name}</h2>
+        <Cards key={p.name}>
+            <h2>{p.name}</h2>
             <p>Height: {p.height}</p>
             <p>Mass: {p.mass}</p>
+            <p>Gender: {p.gender}</p>
             <p>Hair color: {p.hair_color}</p>
         </Cards> )
       })
