15638e63b96b96d8be4cae17cc6baa770f99689e
Eric <ericmichaelrice@protonmail.com>
2020-07-31 20:43:03 -0700

    useState/useEffect hook

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
