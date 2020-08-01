8f0bdbf34333e523f85719699f9f85dca6415469
Bill Xiong <xiong.bill@yahoo.com>
2020-08-01 16:46:31 -0700

    added

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
+import character from './components/Character'
 
 const App = () => {
+  const [characters, setCharacters] = useState([])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,9 +12,21 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+useEffect(() => {
+  axios.get('https://https://swapi.py4e.com/api/species/1/')
+  .then((res) => {
+    setCharacters(response.data.results)
+    console.log(response.data.results)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+})
+}, [])
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <characters characters={characters}/>
     </div>
   );
 }
