884bf7294ff038281466322b59be14f166d60143
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2020-07-31 21:42:51 -0400

    object

--- starwars/src/index.js
+++ starwars/src/index.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import axios from "axios"
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+export default function Characters() {
+    const [starWarsList, setList] = useState([])
+        useEffect(() => {
+            axios.get('https://swapi.dev/api/people/')
+            .then(response => {
+                console.log(response)
+                setList(response.data)
+          })
+            .catch(error => console.log(error))
+        }, [])
+        
+            return (
+              <div className="star-wars-list">
+                  {/* {starWarsList.map(pic => <img>{pic.img}</img>)} */}
+              </div>
+        )
+    }
+    
