fb4244a1962fecd8f603426f98498f432c9f3a83
Aaron Flowers <flowersa29@gmail.com>
2020-08-01 13:58:39 -0500

    8/1

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
+import Character from './components/Character';
+
+
 
 const App = () => {
+  const [state, setState] = useState(0);
+  const [otherState, setotherState] = useState(0)
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+
+  useEffect(() => {
+    console.log("test")
+  }, [])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -12,7 +23,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <h1>hello</h1>
     </div>
+    <Character/>
   );
 }
 
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,10 @@
 // Write your Character component here
+import react from "react";
+
+function Character () => {
+
+return <div className=""></div>
+    
+}
+
+export default Character;
