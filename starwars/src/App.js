99f346329b4eb6c1f20f2a95bb89f98d2433ff5b
David Veele <you@example.com>
2020-07-31 23:48:04 -0400

    could not get map to work.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import './App.css';
+/** @format */
+
+import React from "react";
+//import axios from 'axios';
+import "./App.css";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars API</h1>
+      <Character />
     </div>
   );
-}
+};
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,36 @@
-// Write your Character component here
+/** @format */
+
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import CharacterStats from "./CharacterStats";
+
+const Character = (props) => {
+  const [characterData, setCharacterData] = useState([]);
+  const array = array.from(characterData);
+
+  useEffect(() => {
+    Axios.get("https://swapi.dev/api/people/1/")
+      .then((response) => {
+        console.log("Success", response);
+        setCharacterData(response.data.results);
+      })
+      .catch((error) => console.log("Failure", error));
+  }, []);
+
+  return (
+    <div className="character">
+      {array.map((props) => {
+        return (
+          <CharacterStats
+            key={props.key}
+            name={props.name}
+            height={props.height}
+            mass={props.mass}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Character; // Write your Character component here
