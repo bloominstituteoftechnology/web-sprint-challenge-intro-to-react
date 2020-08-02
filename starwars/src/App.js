50240ee70698a9189479b6b29ddb9a9d7d2475ff
Aaron Flowers <flowersa29@gmail.com>
2020-08-02 00:34:38 -0500

    8/2

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -5,16 +5,29 @@ import Character from './components/Character';
 
 
 
-const App = () => {
-  const [state, setState] = useState(0);
+const App = ()=> {
+  const [people, setPeople] = useState([]);
   const [otherState, setotherState] = useState(0)
 
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   useEffect(() => {
-    console.log("test")
+    axios
+        .get("https://swapi.dev/api/people/")
+        .then(response => {
+           setPeople(response.data.results)
+          console.log(response, "consolelog from appjs")
+        })
+        .catch(error => {
+          console.log(error.message)
+        })
+    
   }, [])
+
+  // useEffect(() =>{
+  //   console.log("people have changed", people)
+  // }, [people])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -22,10 +35,17 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
-      <h1>hello</h1>
+      <h1 className="Header">React Wars</h1>
+      <ul>
+      {people.map(people =>{
+        return <Character key={people.name + people.birth_year} name={people.name}/>
+      })}
+      </ul>
+      
+
     </div>
-    <Character/>
+  
+   
   );
 }
 
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,9 +1,34 @@
 // Write your Character component here
-import react from "react";
+import React from "react";
 
-function Character () => {
+function Character(props) {
 
-return <div className=""></div>
+return (
+
+    <li className="character-info">
+
+        <h2>Character Name: {props.name}</h2>
+        <p>Mass:{props.mass}</p>
+        <p>height:{props.height}</p>
+        <p>gender:{props.gender}</p>
+        <p>eye_color:{props.eye_color}</p>
+        <p>birth_year:{props.birth_year}</p>
+
+        
+        
+
+    
+    
+    
+    
+    
+    
+    </li>
+
+)
+
+
+
     
 }
 
