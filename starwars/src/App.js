4a94fd2866d6a4dfa9f2e354b5ff7c5381ffb2b6
sirbaconsalot <donavynhaley@gmail.com>
2020-08-02 19:24:15 -0700

    Met MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import './App.css';
-
+import Character from './components/Character.js'
+import styled from 'styled-components';
+const axios = require('axios');
+const WrapperDiv = styled.div `
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -8,10 +15,41 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("");
 
+  useEffect(() => {
+    const fetchData = () => {
+      axios
+        .get("https://swapi.dev/api/people/")
+        .then(result => {
+          setData(result.data.results)
+          console.log(result.data.results[0])
+        })
+        .catch(error => {
+          console.log("error: " + error)
+        })
+      };
+
+    fetchData();
+  }, [query]);
+  
+  if(data.length == 0){
+    console.log("loading")
+    return <h1>Loading</h1>
+  }
+  console.log(data)
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <WrapperDiv>
+        {
+          data.map((character) => {
+            return<Character name={character.name} birthyear={character.birth_year}>
+            </Character>
+          })
+        }
+      </WrapperDiv>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+const WrapperDiv = styled.div `
+border: 1px solid red;
+background-color: grey;
+border-radius: 5px;
+color: white;
+width: 50%;
+margin: 1%;
+`;
+function Character(props){
+    const {name, birthyear } = props;
+    console.log(name)
+    
+    return(
+        <WrapperDiv>
+            <h2>Name: {name}</h2>
+            <h2>Birthyear: {birthyear}</h2>
+        </WrapperDiv>
+    )
+}   
+
+export default Character;
