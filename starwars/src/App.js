5e6b858a4e326db59ac2148c7634403e21669ff9
Ryan Clark <rbclark244@gmail.com>
2020-08-02 22:03:11 -0400

    writing first function to send each character out

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Character from "./components/Character";
+import "./App.css";
 
-const App = () => {
+function App() {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [info, setInfo] = useState({});
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((response) => {
+        console.log("success", response);
+        setInfo(response.data);
+      })
+      .catch((error) => console.log("failure", error));
+  }, []);
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">
+        <Character data={info} />
+      </h1>
     </div>
   );
 }
 
 export default App;
+
+// import React from "react";
+// import axios from "axios";
+
+// function App() {
+//   const [info, setInfo] = useState({});
+
+//   useEffect(() => {
+//     axios
+//       .get("https://swapi.dev/api/people/")
+//       .then((response) => {
+//         console.log("success", response);
+//         setInfo(response.data);
+//       })
+//       .catch((error) => console.log("failure", error));
+//   }, []);
+
+//   return <div></div>;
+// }
+
+// export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+import React from "react";
+
+const Character = (props) => {
+  const { data } = props;
+  let mappedData = data.map((element) => {
+    let resultsMapped = element.results;
+    return resultsMapped;
+  });
+  return <div>{resultsMapped}</div>;
+};
+
+export default Character;
