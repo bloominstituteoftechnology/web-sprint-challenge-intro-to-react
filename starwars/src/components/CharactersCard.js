08a3ed31140921076453ad75c7a512d233fbe755
Eric <ericmichaelrice@protonmail.com>
2020-08-02 19:43:08 -0700

    Mapping images

--- starwars/src/components/CharactersCard.js
+++ starwars/src/components/CharactersCard.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import { Button } from 'reactstrap';
+
 
 
 const CharacterCard = props => {
     return (
         <ul>
-                <h3>{props.name}</h3>
-           
-                <p>Height: {props.height} </p>
+                <Button class="p-3 mb-2 bg-primary text-white">{props.name}</Button>
+                <p class=".text-white">Height: {props.height} cm, Weight: {props.mass} kg Birth year: {props.birthYear}</p>
+                {/* <p>Height: {props.height} </p>
                 <p>Mass: {props.mass}</p> 
                 <p>Birth Year: {props.birthYear} </p>
-                <p>Star Ships: {props.starShips}</p> 
-                <p>{props.homeWorld}</p>
+                <p>Star Ships: <img src={props.starShips} /></p>  */}
+                {/* <img src={props.homeWorld} /> */}
 
             
         </ul>
