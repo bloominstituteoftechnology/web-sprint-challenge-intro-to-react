7f99bad9cbfde623fe6c82055926e7f34113e95c
David Veele <you@example.com>
2020-08-03 01:01:42 -0400

    got all the info to display all that is left is styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,21 +1,11 @@
-/** @format */
-
 import React from "react";
-//import axios from 'axios';
 import "./App.css";
 import Character from "./components/Character";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
   return (
     <div className="App">
-      <h1 className="Header">Star Wars API</h1>
+      <h1 className="Header">Characters</h1>
       <Character />
     </div>
   );
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,36 +1,38 @@
-/** @format */
 
 import React, { useState, useEffect } from "react";
-import Axios from "axios";
-import CharacterStats from "./CharacterStats";
+import axios from "axios";
+import CharacterCard from "../components/CharacterCard";
 
-const Character = (props) => {
-  const [characterData, setCharacterData] = useState([]);
-  const array = array.from(characterData);
+const Character = () => {
+  //declaring state
+  const [character, setCharacter] = useState([]);
 
   useEffect(() => {
-    Axios.get("https://swapi.dev/api/people/1/")
+    axios
+      .get("https://swapi.py4e.com/api/people")
       .then((response) => {
-        console.log("Success", response);
-        setCharacterData(response.data.results);
+        // reference setState function
+        console.log(response);
+        setCharacter(response.data.results);
       })
-      .catch((error) => console.log("Failure", error));
+      .catch((error) => console.log("error", error));
   }, []);
-
   return (
     <div className="character">
-      {array.map((props) => {
-        return (
-          <CharacterStats
-            key={props.key}
-            name={props.name}
-            height={props.height}
-            mass={props.mass}
-          />
-        );
-      })}
+      {character.map((character) => (
+        <CharacterCard
+          className="card"
+          key={character.id}
+          name={character.name}
+          birth={character.birth_year}
+          height={character.height}
+          mass={character.mass}
+          hair={character.hair_color}
+          home={character.homeworld}
+        />
+      ))}
     </div>
   );
 };
 
-export default Character; // Write your Character component here
+export default Character;
