f4df458209fdd5666520f931d47072bc4f8d0aa3
gitnubl33t <jessicad8695@gmail.com>
2020-08-29 10:22:48 -0700

    successful response through Axios

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -11,6 +11,17 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const axios = require("axios");
+
+  axios
+    .get("https://rickandmortyapi.com/api/character")
+    .then((res) => {
+      console.log("This is the response:", res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
