43dd4ceb1422b18f847d3653d5710307d9dce06c
gitnubl33t <jessicad8695@gmail.com>
2020-08-29 10:18:11 -0700

    installed axios through npm

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // const [charList, setCharList] = useState();
+
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
@@ -14,6 +16,6 @@ const App = () => {
       <h1 className="Header">Characters</h1>
     </div>
   );
-}
+};
 
 export default App;
