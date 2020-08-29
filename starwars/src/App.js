87f78f5042208904d1b080d5218f435dd7a96378
gitnubl33t <jessicad8695@gmail.com>
2020-08-29 11:21:09 -0700

    Made basic CharacterCard component

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -15,7 +15,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      <CharacterList />
+      {/* <CharacterList /> */}
     </div>
   );
 };
---
+++ starwars/src/components/CharacterCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CharacterCard = ({ char }) => {
+  <div>
+    <h2>Character Name: {char.name}</h2>
+    <p>Status: {char.status}</p>
+  </div>;
+};
+
+export default CharacterCard;
