121723a3ecbde5544bd515d4242c5905ed1fc2ae
gitnubl33t <jessicad8695@gmail.com>
2020-08-29 12:30:56 -0700

    Set the color of Card div, rounded the corners

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,13 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+import { Card } from "../styled-components/Card";
 
 const CharacterCard = ({ char }) => {
   return (
-    <div>
+    <Card>
       <h2>{char.name}</h2>
       <p>Status: {char.status}</p>
       <p>Gender: {char.gender}</p>
       <p>Species: {char.species}</p>
-    </div>
+    </Card>
   );
 };
 
--- starwars/src/styled-components/Card.js
+++ starwars/src/styled-components/Card.js
@@ -2,5 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 export const Card = styled.div`
-  background-color: white;
+  background-color: rgba(179, 78, 94, 0.8);
+  margin-left: auto;
+  margin-right: auto;
+  width: 40%;
+  border-radius: 25px;
 `;
