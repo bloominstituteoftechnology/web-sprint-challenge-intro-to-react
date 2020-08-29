5cd921a50246a1e71b7e0f07076602ee0f1a3473
Tom Harris <tom_3rd_2000@yahoo.com>
2020-08-28 21:14:16 -0400

    working on get data from API and mapping to the function for the Dom, src/components/Character.js

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,3 +1,39 @@
 // Write your Character component here
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import People from "./People"
+
+const Characters = props => {
+
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get ("https://swapi.dev/api/people/")
+                .then(res => {
+                    console.log(res);
+                        setCharacters(res.data.results);
+            })
+                .catch(error => {
+                    console.log("StarWars not Working Browswer has Covid-19", error);
+                });
+    },[])
+
+    console.log(characters);
+
+    return (
+        <div>
+            {characters.map(character => {
+               return (<People 
+                // key = {character.url} 
+                name ={character.name}
+                home ={character.homeworld} 
+                species ={character.species}
+                />);
+            })}
+        </div>
+    );
+
+};
+ 
+export default Characters;
