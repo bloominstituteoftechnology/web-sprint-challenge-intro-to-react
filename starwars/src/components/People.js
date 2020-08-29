bbd4fed83769b3dfafc47e5b3f73b0c207836ea2
Tom Harris <tom_3rd_2000@yahoo.com>
2020-08-28 21:12:32 -0400

    working on people list, src/components/People.js

---
+++ starwars/src/components/People.js
@@ -0,0 +1,13 @@
+import React, {useState } from "react";
+
+const People = props => {
+    return (
+        <div>
+            <h2>{props.name}</h2>
+            <p>{props.homeworld}</p>
+            <p>{props.species}</p>
+        </div>
+    );
+};
+
+export default People;
