9e4a79c7323f9093f6883575cebeaec2316b4d16
matieuruth <matieu.ruth@gmail.com>
2020-08-30 19:27:40 -0400

    mapped over character list

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -20,6 +20,14 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map((item) => {
+        return(
+          <Character
+            name={item.name}
+            url={item.url}
+          />
+        )
+      })}
     </div>
   );
 }
