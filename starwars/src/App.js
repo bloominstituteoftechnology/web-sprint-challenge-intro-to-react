73ad762ad15b7e7638e270574abeb8b187455b76
Tom Harris <tom_3rd_2000@yahoo.com>
2020-08-30 16:40:16 -0400

    working on imports and default export app, starwars/src/App.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -13,13 +13,13 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [characters, setCharacters] = useState([]);
+  const [data, setData] = useState([]);
   useEffect(() => {
   axios
   .get ("https://swapi.dev/api/people/")
   .then(res => {
-      console.log(res);
-  setCharacters(res.data.results);
+      console.log("Here is the FORCE! Data", res.data.results);
+  setData(res.data.results);
 })
   .catch(error => {
       console.log("StarWars not Working Browswer has Covid-19", error);
