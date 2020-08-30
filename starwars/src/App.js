e496ff872b985b1f589197834d5a56019cae0a84
Tom Harris <tom_3rd_2000@yahoo.com>
2020-08-30 16:27:49 -0400

    repairing files, starwars/src/App.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 import Characters from './components/Character';
 
@@ -11,6 +13,20 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+  axios
+  .get ("https://swapi.dev/api/people/")
+  .then(res => {
+      console.log(res);
+  setCharacters(res.data.results);
+})
+  .catch(error => {
+      console.log("StarWars not Working Browswer has Covid-19", error);
+});
+},[])
+
+      console.log(characters);
 
   return (
     <div className="App">
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -5,21 +5,20 @@ import People from "./People"
 
 const Characters = props => {
 
-    const [characters, setCharacters] = useState([]);
-
-    useEffect(() => {
+const [characters, setCharacters] = useState([]);
+        useEffect(() => {
         axios
-            .get ("https://swapi.dev/api/people/")
-                .then(res => {
-                    console.log(res);
-                        setCharacters(res.data.results);
-            })
-                .catch(error => {
-                    console.log("StarWars not Working Browswer has Covid-19", error);
-                });
-    },[])
+        .get ("https://swapi.dev/api/people/")
+        .then(res => {
+            console.log(res);
+        setCharacters(res.data.results);
+    })
+        .catch(error => {
+            console.log("StarWars not Working Browswer has Covid-19", error);
+    });
+},[])
 
-    console.log(characters);
+            console.log(characters);
 
     return (
         <div>
