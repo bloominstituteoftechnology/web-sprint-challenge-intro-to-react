f2fd8f47fd4ac4d629598cacffafff5291f92a64
matieuruth <matieu.ruth@gmail.com>
2020-08-30 19:23:49 -0400

    initiated api call

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  
+  const [characters, setCharacters] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/')
+      .then((res)=>{
+        console.log('response ', res)
+        setCharacters(res.data.results)
+      })
+      .catch((err)=>{
+        console.log('error: ', err)
+      })
+  }, []);
+  
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
