26dc5d4fcc2563ef7f839bd8241b74e243990db4
matieuruth <matieu.ruth@gmail.com>
2020-08-30 19:37:14 -0400

    imported styled components into App

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import styled from 'styled-components';
+
 import axios from 'axios';
 import Character from './components/Character';
 
