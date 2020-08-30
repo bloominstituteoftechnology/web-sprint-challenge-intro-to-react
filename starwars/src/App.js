6ee82b41d2db3be7eb3ba2771f084c1575dcb77b
matieuruth <matieu.ruth@gmail.com>
2020-08-30 19:32:23 -0400

    created Character comp

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
+import Character from './components/Character';
 
 const App = () => {
   
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,11 @@
-// Write your Character component here
+import React from 'react';
+
+function Character(props){
+    return (
+        <div>
+            <p>{props.name}</p>
+        </div>
+    )
+}
+
+export default Character;
