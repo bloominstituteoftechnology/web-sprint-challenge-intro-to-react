ee1e1528ce513bfdd320e35e4f718b61ef85c329
unknown <ngriffith29@student.egcc.edu>
2020-08-30 19:58:55 -0400

    setup app.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import CharacterMap from './components/CharacterMap'
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [data,setData] = useState([])
+  useEffect(() => {
+   
+    // Update the document title using the browser API
+    axios.get('https://swapi.dev/api/people/')
+  .then(function (response) {
+    
+    setData(response.data.results)
+    
+
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+  }, []);
+
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -11,7 +30,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+   
     </div>
   );
 }
