ab482b701c88aea26229bdf736c1af8e0aa47148
Damaris Garcia <damaris-garcia@lambastudents.com>
2020-08-30 19:16:17 -0400

    State, Effect and Axios Call Set

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import Character from "./components/Character";
+import axios from "axios";
 import './App.css';
 
 const App = () => {
@@ -8,6 +10,19 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.py4e.com/api/people")
+      .then(res => {
+        console.log(res, "Response Sucessful");
+        setCharacters(res.data.results);
+      })
+      .catch(error => {
+        console.log(error, "Sorry Not Now");
+      });
+  }, []);
 
   return (
     <div className="App">
