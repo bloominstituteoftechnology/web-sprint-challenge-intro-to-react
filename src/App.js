40213fea0f672b9f1da33752ff8b08ae431f8dc0
Prince Buduru <princebuduru1995@gmail.com>
2020-09-11 13:29:47 -0400

    getting there

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
@@ -8,11 +9,53 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  
+  const [url, setUrl] = useState('https://rickandmortyapi.com/api/character/?name=')
+  const [info, setInfo] = useState({})
+  const [results, setResults] = useState([])
+  const [search, setSearch] = useState('')
+
+
+  
+  useEffect(()=> {
+    console.log('url: ', url)
+    console.log('info: ', info)
+    console.log('results: ', results)
+    console.log('search: ', search)
+    console.log(url, info, results)
+  }, [url, info, results])
+
+
+  useEffect(() => {
+    axios.get(`${url} ${search}`)
+    .then((result) => {
+      console.log(result)
+      setInfo(result.data.info)
+      setResults(result.data.results)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }, [])
 
   return (
+    <>
+    <form>
+      <input onChange={(e) =>{
+        setSearch=e.target.value
+      }}
+      value={search}
+      type='text'/>
+    </form>
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      {results.map((result, index)=> (
+        <>
+        <img src={result.image} alt={`Photo of ${result.name}`} />  
+        <p key={index}>{result.name}</p>
+        </>
+      ))}
     </div>
+    </>
   );
 }
 
