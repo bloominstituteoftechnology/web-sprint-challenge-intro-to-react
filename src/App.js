8b04179f73984861dcf237cf88a47620356fe0d1
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro.local>
2020-09-11 13:58:55 -0400

    mvp met

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Character from "./components/Character"
 import './App.css';
+import Axios from 'axios';
 
 const App = () => {
+  const [baseUrl] = useState("https://rickandmortyapi.com/api/character/")
+ 
+  const [characters, setCharacters] = useState([
+    {
+      image: "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+      name: "Abandango Cluster"
+    }
+  ])
+
+  useState(() => {
+   
+    Axios.get(baseUrl)
+    .then((info) => {
+      console.log(info)
+      setCharacters(info.data.results)
+    })
+    .catch((err) =>{
+      console.log("there is an error", err)
+    })
+  }, [])
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -11,7 +34,14 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Rick and Morty Characters</h1>
+  
+      {
+        characters.map((character, index) => (
+          <Character key= {`app-character-map-${index}`} character={character} />
+        ))
+        }
+
     </div>
   );
 }
