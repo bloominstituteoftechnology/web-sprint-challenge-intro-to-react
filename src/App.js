cb2817645cf8bbcd6ae1e2a6949dcbecefafb70c
Christopher Barrett <municipalshock@gmail.com>
2020-09-13 12:34:29 -0700

    render characters to screen

--- src/App.js
+++ src/App.js
@@ -6,28 +6,29 @@ import axios from "axios";
 import "./App.css";
 
 const App = () => {
-  const [character, setCharacter] = useState([]);
-  const URL = `https://swapi.dev/`;
+  const [characters, setCharacters] = useState([]);
+  const URL = `https://swapi.dev/api/people/`;
 
   useEffect(() => {
     axios
       .get(URL)
       .then((res) => {
-        console.log("response", res.data);
+        console.log("response", res.data.results);
+        setCharacters(res.data.results);
       })
       .catch((err) => console.log(err));
   }, []);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a
+  // Fetch character from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      <Character character={"character info"} />
+      <Character characters={characters} />
     </div>
   );
 };
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,14 +1,47 @@
 // Write your Character component here
 import React from "react";
-// import cuid from "cuid";
-// import styled from "styled-components";
+import cuid from "cuid";
+import styled from "styled-components";
 
-const Character = (props) => {
-  console.log("props", props);
+const StyledDiv = styled.div`
+  background-color: ${(pr) => pr.theme.black};
+  color: ${(pr) => pr.theme.white};
+  padding: ${(pr) => pr.theme.paddingLarge};
+  margin: ${(pr) => pr.theme.marginSmall} auto;
+  border-radius: 10px;
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+
+  h2 {
+    font-size: 3rem;
+    text-align: center;
+  }
+  h2:hover {
+    color: goldenrod;
+  }
+  p {
+    line-height: 1.5;
+  }
+  p:hover {
+    color: purple;
+  }
+`;
+
+const Character = ({ characters }) => {
+  // console.log("props", props.characters);
   return (
-    <div>
-      <h2>{props.character}</h2>
-    </div>
+    <>
+      {characters.map((character) => (
+        <StyledDiv>
+          <h2>NAME:{character.name}</h2>
+          <p>
+            Height:{character.height} Mass:{character.mass} Hair:
+            {character.hair_color} Skin:{character.skin_color}
+          </p>
+        </StyledDiv>
+      ))}
+    </>
   );
 };
 
