64841a0e971f16abadda96ca6cad6f8e253dd16b
Aja Blanco <aja@Ajas-MacBook-Pro.local>
2020-09-24 20:25:33 -0700

    mvp solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Character from "./components/Character";
+import styled from "styled-components";
+
+const Card = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 30px;
+  align-items: center;
+  padding: 10px;
+  backgroundcolor: white;
+  color: black;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [character, setCharacter] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20")
+      .then((res) => {
+        console.log("res.data", res.data.results);
+        setCharacter(res.data.results);
+      })
+      .catch((err) => console.log("err", err));
+  }, []);
+
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div className="App" style={{display: "flex", flexWrap: "wrap", justifyContent: "space-evenly"}}>
+      <div style={{width: "100%"}}>
+        <h1 style={{textAlign: "center", color: "white"}}>Pokemon Characters</h1>
+      </div>
+      {character.map((char, index) => {
+        return <Character key={index} character={char} />;
+      })}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,41 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const Card = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 30%;
+  justify-content: space-evenly;
+  margin-top: 30px;
+  align-items: center;
+  padding: 20px;
+  background-color: gold;
+  color: black;
+  margin: 5%;
+  border-radius: 6px;
+`;
+
+const Button = styled.button`
+    color: white;
+    background-color: black;
+    border-radius: 6px;
+    padding: 5%;
+    textAlign: "center";
+    width: 100%;
+`
+
+
+const Character = ({ character }) => {
+  console.log("poke props", character);
+  return (
+      <Card>
+        <h2 style={{textAlign: "center", fontSize: 30, width: "100%"}}>Name: {character.name} </h2>
+        <br/>
+        <Button onClick={()=> {window.open(character.url)}}>Learn More</Button>
+      </Card>
+      
+  );
+};
+
+export default Character;
