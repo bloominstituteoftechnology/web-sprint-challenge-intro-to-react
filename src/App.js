67a87cba9f2cd8e3a2863625b553d42cfef88a98
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-09-26 11:42:38 -0700

    Name prop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
+import styled from "styled-components";
 import './App.css';
+import Character from './components/Character';
+import axios from 'axios';
+
+const urlB = ["https://swapi.dev/api/"]
+const thing = ["people/"]
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,12 +15,39 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [count,setCount] = useState(3);
+  const [reqG, setReqG] = useState({});
+  
+
+  useEffect( () =>{
+    axios.get(urlB[0]+thing[0]+count+"/")
+    .then( (re) =>{
+      
+        setReqG(re.data);
+      
+        // setReqG(re.data);
+      
+      console.log(re.data);
+    })
+    .catch( (er1) =>{
+      console.log(er1);
+    })
+  },{})
+
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+   <div>
+
+
+      {
+
+        <Character key={reqG} name={reqG.name} />
+        
+      }
+          
+
+   </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,30 @@
 // Write your Character component here
+/*
+
+ Obtain a list of characters. One or several requests might be needed, depending on the API.
+Set the list of characters into state.
+Render your characters to the screen:
+
+    Build a React component named 'Character' to render an individual character.
+    Map over the list in state, and for each character render a Character to the page.
+    You must display at least one element for each character in the data set.
+    The elements must be styled with styled-components - don't rely on browser default styles.
+
+*/
+
+import React,{useState, useEffect } from "react";
+import axios from "axios";
+
+const Character = ({name}) =>{
+
+
+    if(!name) return <h2>Loading...</h2>;
+    return (
+        <div>
+            <h3>{name}</h3>
+        </div>
+    );
+};
+
+
+export default Character;
