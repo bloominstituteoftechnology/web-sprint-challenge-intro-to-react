c04057b15c79979f7103725b56336e8e2c3247e6
mbenson3434 <44735865+mbenson3434@users.noreply.github.com>
2020-10-09 18:07:07 -0400

    MVP done

--- src/App.js
+++ src/App.js
@@ -5,39 +5,27 @@ import axios from 'axios';
 
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+ 
   const [characters, setCharacters] = useState([])
-  
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
-  // 
-  // Grab the data
+ 
   useEffect(() => {
     axios.get(`https://swapi.dev/api/people/`)
       .then(res => {
-        console.log(res)
-        setCharacters(res.data.results)
-        
+        setCharacters(res.data.results) 
       })
       .catch(err => {
         console.log(err)
       })
   }, [])
-console.log(characters)
-characters.map(item => {
-  console.log(item.mass);
-})
-
+  
+  
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
       {
         characters.map(char => {
-          return <Character char={char} />
+          return <Character key={char} charNames={char} mass={char.mass} height={char.height} hair={char.hair_color} eyes={char.eye_color} />
         })
       }
     </div>
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,6 +1,7 @@
 // Write your Character component here
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+
 
 const StyledCharacter = styled.button`
   color: black;
@@ -11,23 +12,55 @@ const StyledCharacter = styled.button`
   justify-content: center;
   align-items: center;
 `
-const CharDiv = styled.div`
-    display: flex;
-    justify-content: center;
-    width: 50%;
+const DetailsDiv = styled.div`
+  background-color: white;
+  color: blue;
+  width: 200px;
+  text-align: left;
+  
 `
 
-export default function CharacterName(props) {
-    const { char } = props
+const CharDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center; 
+    align-items: center;
+`
+
+export default function Character(props) {
+    const { 
+      charNames,
+      mass,
+      height,
+      hair,
+      eyes 
+      } = props
+    
+    const [toggle, setToggle] = useState(true)  
+    
+    if (toggle) {
+      return (
+        <CharDiv> 
+          <StyledCharacter onClick = {event => {setToggle(false)}}>
+          {charNames.name}
+          </StyledCharacter>
+        </CharDiv>
+      )
+    } 
+
 
     return (
-        <CharDiv>
-            <StyledCharacter>
-              {char.name}
-            </StyledCharacter>
-        </CharDiv>
+      <CharDiv>
+        <StyledCharacter onClick = {event => {setToggle(true)}}>
+        {charNames.name}
+        </StyledCharacter>
+        <DetailsDiv>   
+          <h2>Mass: {mass}</h2>
+          <h2>Height: {height}</h2>
+          <h2>Hair Color: {hair}</h2>
+          <h2>Eye Color: {eyes}</h2>
+        </DetailsDiv>
+      </CharDiv>  
     )
 }
-
-//const Details = (props) => {
-//}
+ 
