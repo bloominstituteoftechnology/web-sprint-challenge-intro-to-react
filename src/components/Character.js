a87c42b8279da6dc28f5350df2dad0a280d0ed58
NDCT94 <nathan.dumas01@gmail.com>
2020-10-09 12:12:43 -0400

    data displayed, styling now

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,21 @@
 // Write your Character component here
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
+
+export default function Character(props) {
+    return (
+        <div>
+            {props.chData.map(item => {
+                return (
+                    <div>
+                        <h1>{item.name}</h1>
+                        <h2>Gender: {item.gender}</h2>
+                        <h2>Height: {item.height}</h2>
+                        <h2>Hair color: {item.hair_color}</h2>
+                        <h2>From planet: {item.homeworld}</h2>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
