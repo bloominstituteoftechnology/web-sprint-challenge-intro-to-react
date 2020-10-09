8883eb856585e55d6d532e09434b61a7edbb63bb
mbenson3434 <44735865+mbenson3434@users.noreply.github.com>
2020-10-09 11:39:12 -0400

    Readme Qs answered

---
+++ build/precache-manifest.cb94825fad5b06f1e62fd56152e55b66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7175fe1ef7f19cad00670f0983e8e79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "574e48a5e28622a4bfa1",
+    "url": "/static/css/main.87c00a12.chunk.css"
+  },
+  {
+    "revision": "bc1b01628c862e0f02e0",
+    "url": "/static/js/2.fc079596.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fc079596.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "574e48a5e28622a4bfa1",
+    "url": "/static/js/main.665b9e80.chunk.js"
+  },
+  {
+    "revision": "57fca467b55bfed9c5ca",
+    "url": "/static/js/runtime-main.f688ac41.js"
+  },
+  {
+    "revision": "dff75a1ffda792b2380226033f28415c",
+    "url": "/static/media/sw-bg.dff75a1f.jpg"
+  }
+]);
