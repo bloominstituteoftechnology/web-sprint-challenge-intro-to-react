33947a3b13cd9efb67db5a3903615ff76d36a86a
Maksim Raymond <maksim-raymond@lambdastudents.com>
2020-10-09 14:30:22 -0700

    stuck

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,11 +1,18 @@
 // Write your Character component here
 
 import React, {useState} from 'react';
-
-
-function character{
-
+const listOfCharacters [
+{"id":2,
+"name":"Morty Smith",
+"status":"Alive",
+"species":"Human",
+"type":"",
+"gender":"Male",
+"origin":{"name":"Earth (C-137)","url":"https://rickandmortyapi.com/api/location/1"},
+"location":{"name":"Earth (Replacement Dimension)",
+"url":"https://rickandmortyapi.com/api/location/20"},
+"image":"https://rickandmortyapi.com/api/character/avatar/2.jpeg"
 }
+]
 
-const [devs, setDevs] = useState(listOfAwesome)
-const [ids, setIds] = useState(null)
+const[character, setCharacter] = useState()
