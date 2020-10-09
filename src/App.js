59d1021b6e2fb73b34507164b90fadb7a816eb16
mbenson3434 <44735865+mbenson3434@users.noreply.github.com>
2020-10-09 13:47:42 -0400

    name buttons up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Character from './components/Character';
+import axios from 'axios';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [characters, setCharacters] = useState([])
+  
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  // 
+  // Grab the data
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people/`)
+      .then(res => {
+        console.log(res)
+        setCharacters(res.data.results)
+        
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+console.log(characters)
+characters.map(item => {
+  console.log(item.mass);
+})
+
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        characters.map(char => {
+          return <Character char={char} />
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,33 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledCharacter = styled.button`
+  color: black;
+  background-color: white;
+  font-weight: bold;
+  width: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const CharDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 50%;
+`
+
+export default function CharacterName(props) {
+    const { char } = props
+
+    return (
+        <CharDiv>
+            <StyledCharacter>
+              {char.name}
+            </StyledCharacter>
+        </CharDiv>
+    )
+}
+
+//const Details = (props) => {
+//}
