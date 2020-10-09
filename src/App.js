8443493e8612e748ba5f3a1b50498a2f7433dd95
Cameron Garner <camerongarner9@gmail.com>
2020-10-09 13:44:38 -0400

    still working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import axios from "axios"
+//import Character from "./components/Character"
+
 
 const App = () => {
+
+  let [swcharacters,setswcharacters] = useState([])
+
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+useEffect(function(){
+axios.get("https://swapi.dev/api/people/")
+.then(function(swdata){
+ setswcharacters(swdata.data.results)
+})
+},[])
+
+ console.log(swcharacters)
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+ <div>
+   {swcharacters.map(function(item){
+     
+   })}
+ </div>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+let Character = function(props){
+let {} = props;
+return(
+<div>
+
+</div>
+);
+}
+
+
+export default Character
