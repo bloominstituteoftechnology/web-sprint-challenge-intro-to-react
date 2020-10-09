de01b591c6e838f2d52a0de3fe68ad4f1d388b79
Maksim Raymond <maksim-raymond@lambdastudents.com>
2020-10-09 11:10:19 -0700

    errors

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,11 @@
 // Write your Character component here
+
+import React, {useState} from 'react';
+
+
+function character{
+
+}
+
+const [devs, setDevs] = useState(listOfAwesome)
+const [ids, setIds] = useState(null)
