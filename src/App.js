bbfab2ce2c325a13c86d54d71f9c668d22f9a56b
Trey Roberts <treywroberts@gmail.comgit config --global user.name TreywRobertsrungit.addgit commit -m added>
2020-10-09 18:27:01 -0400

    I'm getting it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
-import Character from './components/Character';
+import {Character} from './components/Character';
+import styled from 'styled-components'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -10,66 +11,32 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [people, setPeople] = useState([])
-  const [people2, setPeople2] = useState([])
-  const [people3, setPeople3] =useState([])
+  const [data, setData] =useState([])
 
-  useEffect(() => {
-    axios.get(`https://swapi.py4e.com/api/people/1/`)
-      .then(res => {
-        setPeople(res.data)
-        console.log(res.data)
-        console.log(people)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }, [])
-  useEffect(() => {
-    axios.get(`https://swapi.py4e.com/api/people/2/`)
-      .then(res => {
-        setPeople2(res.data)
-        console.log(res.data)
-        console.log(people2)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }, [])
-  useEffect(() => {
-    axios.get(`https://swapi.py4e.com/api/people/3/`)
-      .then(res => {
-        setPeople3(res.data)
-        console.log(res.data)
-        console.log(people3)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }, [])
+  useEffect(()=>{
+    axios.get('https://swapi.py4e.com/api/people')
+    .then(res =>{
+      setData(res.data.results)
+    })
+    .catch(err=>{
 
+    })
+  }, [])
 
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
-      <h2>{people.name}</h2>
-      <p>Height:{people.height}</p>
-        <p> Mass: {people.mass}</p>
-         <p> Color:{people.skin_color}</p>.
       
-
-
-      <h2>{people2.name}</h2>
-      <p>Height:{people2.height}</p>
-        <p> Mass: {people2.mass}</p>
-         <p> Color:{people2.skin_color}</p>.
-
-
-      <h2>{people3.name}</h2>
-      <p>Height:{people3.height}</p>
-        <p> Mass: {people3.mass}</p>
-         <p> Color:{people3.skin_color}</p>.
+      <h1 className="Header">Characters</h1>
+      <div className='container'>
+        <div>
+        {data.map(item=>{
+          return <Character key={item.id} data={item}/>
+        
+        })}
+        </div>
+      </div>
+     
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,13 +1,48 @@
 // Write your Character component here
 import React, {useState, useEffect} from 'react'
+import '../App.css';
+import styled from 'styled-components'
+const StyledCharacter = styled.div`
+*{
+    display:flex;
+}
+div{
+    background:skyblue;
+    margin:1%;
+    border-radius:15%;
+    color:white;
+    width:60%;
+    margin-left:20%;
 
-export default function Character({ info, action }) {
-    return (
-      <div  className='character'>
-        {info.name}
-        <button onClick={() => action(info.id)}>
-          See details
-        </button>
-      </div>
+}
+h2{
+    color:black;
+    &:hover{
+        transition: all .5s ease-in-out;
+        color:${pr=>{
+            return pr.evil ? 'red' : 'green'
+        }}
+    }
+    transition: all 1s ease-in-out;
+}
+p{
+    margin-left:5%;
+    align-content:space-around;
+    margin-top: 3%;
+}
+`
+
+export const Character = (props) =>{
+
+    return(
+        <StyledCharacter evil={props.data.name==='Darth Vader'}>
+            <div>
+            <h2>{props.data.name}</h2>
+            <p>Hair Color: {props.data.hair_color}</p>
+            <p>Skin Color: {props.data.skin_color}</p>
+            <p>Weight: {props.data.mass}</p>
+           </div>
+        </StyledCharacter>
+
     )
-  }
+}
