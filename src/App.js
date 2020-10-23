21df0af607ac40529dc5449cc652ea4212bac968
IsaacGHoward <howardisaac100@gmail.com>
2020-10-23 16:51:39 -0700

    Made basic API request to get star wars characters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-
+const axios = require('axios').default;
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -9,9 +9,22 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  //<button onClick={() => setPage(page+1)}>Next page</button>
+  const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
+  useEffect(() => {
+    const fetchCharacters = () => 
+    axios.get(`https://swapi.dev/api/people?page=${page}`)
+    .then(res => {
+      console.log(res.data);
+      setCharacters(res.data);
+    });
+    fetchCharacters();
+  }, [page])
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      
     </div>
   );
 }
