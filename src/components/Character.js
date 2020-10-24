821ea63bb028b8b1583905019eaa90e2cbe0e6df
IsaacGHoward <howardisaac100@gmail.com>
2020-10-23 17:26:36 -0700

    Styled characters

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,14 +1,22 @@
 // Write your Character component here
 import React from 'react';
 import styled from 'styled-components';
+const CharSection = styled.div`
+    background-color: black;
+    width: 800px;
+    color: yellow;
+    border: 2px solid yellow;
+    padding: .75em;
+    font-size: 20px;
+    margin: .5em auto;
+`
 function Character(props){
   console.log(props);
   if(!props.data) return <div className="App">Loading ...</div>
-  
     return (
-      <div>
+      <CharSection>
           {props.data.name}
-      </div>
+      </CharSection>
     );
 }
 export default Character;
--- src/components/CharacterPage.js
+++ src/components/CharacterPage.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import Character from './Character';
+
+
 function CharacterPage(props){
   console.log(props);
    function formatCharacters(chars){
