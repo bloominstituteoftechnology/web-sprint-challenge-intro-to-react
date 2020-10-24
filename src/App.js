e4aafcad8e40b33623b4ac2e9ccc2be518026c3c
milklot <vbiostflex@gmail.com>
2020-10-24 14:02:28 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios"
+import Character from "./Character"
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,25 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [ swData, setSwData ] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then(response => setSwData(response.data.results))
+    .catch(error => console.log("something went wrong: ", error));
+  }, []);
+
+  console.log(swData);
+
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        swData.map((data, index) =>{
+          return <Character key = {index} characterData = {data} />
+        })
+      }
     </div>
   );
 }
