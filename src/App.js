9fd240b79bdca70acd46ab3557134681cea01933
antoine-bibb <ajbibb0114@gmail.com>
2020-10-26 17:49:14 -0400

    intro

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import Character from "./components/Character";
+import "./App.css";
+import Axios from "axios";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [baseUrl] = useState("https://rickandmortyapi.com/api/character/");
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [characters, setCharacters] = useState([
+    {
+      image: "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+      name: "Abandango Cluster",
+    },
+  ]);
+
+  useState(() => {
+    Axios.get(baseUrl)
+      .then((info) => {
+        console.log(info);
+        setCharacters(info.data.results);
+      })
+      .catch((err) => {
+        console.log("there is an error", err);
+      });
+  }, []);
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Rick and Morty Characters</h1>
+
+      {characters.map((character, index) => (
+        <Character key={`app-character-map-${index}`} character={character} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React from "react";
+
+const Character = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <img src={props.character.image} alt={props.character.name} />
+    </div>
+  );
+};
+
+export default Character;
