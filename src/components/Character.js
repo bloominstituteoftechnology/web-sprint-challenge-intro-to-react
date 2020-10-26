3a9e9d5d208810c74c3e088dc3f3a71e89422246
Danielle Koduru <69174710+DanielleKoduru@users.noreply.github.com>
2020-10-25 18:15:44 -0600

    character name and height rendering to screen

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React from "react";
+
+const Character = (props) => {
+    const { name, height } = props.starWars
+    return (
+        <div>
+            <h1>{name}</h1>
+            <h2>{height}</h2>
+        </div>
+    );
+};
+
+export default Character;
