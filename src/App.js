0359ee069e8c752aed4cd6bdbe5b3553bc09245f
Diego Roman <diegormnv209@gmail.com>
2020-11-06 10:04:46 -0800

    Data is displayng on screen

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Character from './components/Character';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [character, setCharacter] = useState();
+  const [characters, setCharacters] = useState();
   
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -15,9 +15,10 @@ const App = () => {
     .get('https://swapi.dev/api/people/')
     .then((res) =>{
       // console.log(res.data.results);
-        for(let i = 0; i <=10; i++){
-        setCharacter(res.data.results[i]);
-        }
+        // for(let i = 0; i <=10; i++){
+        // setCharacter(res.data.results[i]);
+        // }
+        setCharacters(res.data.results)
     })
     .catch((err) =>{
       console.log(err, 'error');
@@ -27,7 +28,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      <Character character={character} />
+      {characters && characters.map(character => {
+        return <Character key={characters.id} character={character}  />
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -5,11 +5,13 @@ const Character = ({ character }) => {
 //   const { character } = props;
 console.log({character})
 
-  return (
-    <div>
-      
-    </div>
-  );
+  return(
+      <div>
+        <div>{character.name}</div>
+        <p>{character.birth_year}</p>
+        <p></p>
+      </div>
+    );
 };
 
 export default Character;
