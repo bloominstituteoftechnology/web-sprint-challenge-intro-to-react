0871f1e017b396677a387b64c6ef63204642229b
Prince Iwejuo <princeiwejuo@gmail.com>
2020-11-06 13:34:37 -0500

    sprint2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+
 
 const App = () => {
+  const [pokeData, setPokeData] = useState({});
+
+  useEffect(() => {
+    const fetchPoke = () => {
+      axios.get('https://pokeapi.co/api/v2/pokemon/ditto')
+        .then(res => {
+          console.log(res.data);
+          setPokeData(res.data);
+        })
+        .catch(err => {
+
+        });
+    };
+    fetchPoke();
+  }, []);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +29,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <h2>{pokeData.name}</h2>
+      <p>{pokeData.height}</p>
+
     </div>
   );
 }
