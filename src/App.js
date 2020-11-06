2fd9202ea61e14568fbd2f1b005344f8b56a75d9
Diego Roman <diegormnv209@gmail.com>
2020-11-06 09:19:53 -0800

    added api and filter to data I need

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import axios from 'axios';
+import Character from './components/Character';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [character, setCharacter] = useState();
+  
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() =>{
+    axios
+    .get('https://swapi.dev/api/people/')
+    .then((res) =>{
+      // console.log(res.data.results);
+        for(let i = 0; i <=10; i++){
+        setCharacter(res.data.results[i]);
+        }
+    })
+    .catch((err) =>{
+      console.log(err, 'error');
+    })
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character character={character} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from "react";
+
+const Character = ({ character }) => {
+//   const { character } = props;
+console.log({character})
+
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Character;
