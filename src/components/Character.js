d6d49ddeec715b99437f6cd5fdaf74cee7da4add
Maksim Raymond <maksim-raymond@lambdastudents.com>
2020-11-06 11:04:47 -0800

    finished

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,18 +1,41 @@
 // Write your Character component here
 
-import React, {useState} from 'react';
-const listOfCharacters [
-{"id":2,
-"name":"Morty Smith",
-"status":"Alive",
-"species":"Human",
-"type":"",
-"gender":"Male",
-"origin":{"name":"Earth (C-137)","url":"https://rickandmortyapi.com/api/location/1"},
-"location":{"name":"Earth (Replacement Dimension)",
-"url":"https://rickandmortyapi.com/api/location/20"},
-"image":"https://rickandmortyapi.com/api/character/avatar/2.jpeg"
-}
-]
+import React from 'react';
+import styled from 'styled-components'
 
-const[character, setCharacter] = useState()
+const Character = props => {
+    const { characterData } = props;
+    
+    const CharacterCard = styled.div`
+        font-size: 1.5rem;
+        background-color: black ;
+        width: 40%;
+        margin: 0 auto;
+        margin-bottom:2%;
+        color: lightgray;
+        padding: 0.2%;
+        border: 3px solid brown;
+    `
+    const CharacterName = styled.h2`
+    color: red;
+    `
+    const CharacterInfo = styled.p`
+    text-align: center;
+    ` 
+    
+    console.log(characterData)
+        return (
+            <CharacterCard>
+                <CharacterName>
+                    {characterData.name}
+                </CharacterName>
+                <CharacterInfo>{`height: ${characterData.height}`}</CharacterInfo>
+                <CharacterInfo>{`mass: ${characterData.mass}`}</CharacterInfo>
+                <CharacterInfo>{`gender: ${characterData.gender}`}</CharacterInfo>
+                <CharacterInfo>{`birth year: ${characterData.birth_year}`}</CharacterInfo>
+            </CharacterCard>
+        )
+    
+    }
+    
+    export default Character;
