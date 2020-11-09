0cc5f8fcf91d85f5d41a6a47609eaaee026d38b9
Katie Olson <katieolson@katies-air.lan>
2020-11-08 22:07:12 -0700

    Completed MVP and Styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Character from './components/Character';
 const App = () => {
   const [characters, setCharacters] = useState([])
 
-
 useEffect (() => {
   axios.get('http://swapi.dev/api/people/')
     .then(res => {
@@ -18,12 +17,10 @@ useEffect (() => {
     })
 }, []);
 
-console.log(characters)
-
-
   return (
     <div className="App">
-      <h1 className="Header">Star Wars Characters</h1>
+      <h1 className="Header"> ===S#T#%ar wa%^R$S+++ </h1>
+      <h2 className="subHeader">CHARACTERS</h2>
       <div className="characterContainer">
           { characters.map((data, index) => {
             return <Character key={index} characterData= {data}/>
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,40 +1,72 @@
-// Write your Character component here
-import React, { useState } from 'react'
-import Details from './Details'
-import styled from "styled-components";
 
-    const Character = ({characterData}) => {
+import React, { useState } from 'react'
+import styled from "styled-components";
+import { Accordion, Card } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.css";
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-content: center;
+    text-align: center;
+
+.card{
+    background-color: #0961AA;
+    opacity: 0.75;
+    font-family: Starwars;
+    margin: 1%;
+    width: 98%;  
+}
+
+.stats{
+    font-size: 3rem;
+    color: #DAAD40;
+    text-shadow: 1px 1px 5px #404040;
+    font-family: Star Jedi Hollow;
+    cursor: pointer;
+   
+}
+.cardBody{
+    background-color: white;
+    font-size: 3rem;
+    
+}
+
+.title{
+    font-family: Starwars;
+    font-size: 5rem;
+    opacity: 1;
+    color: white;
+    text-shadow: 1px 1px 5px #202121;
+    
+}
+`
+const Character = ({characterData}) => {
  const [open, setOpen] = useState(false);
 
- const onChange = () => {
-     if(open === false) {
-         setOpen(true);
-     }
-     else{
-         setOpen(false);
-     }
- }
-
-// console.log(characterDetails[]);
-const Link = styled.a`
-    color: orange;
-    font-size: 2rem;
-    cursor: pointer;
-    &:hover{
-    text-decoration: none;
-    }
-`
-    return (
-        <div className="cardContainer">
-            <div>
-            {characterData.name}
-            <Link onclick={onChange}>Stats</Link>
-            </div>
-            {open && <Details info={characterData.data}/>}
-        </div>
-
-    )
-    
+ return(
+     <Container>
+     <Accordion defaultActiveKey="0">
+         <Card className= "card">
+                <Card.Title className="title"> {characterData.name}</Card.Title>
+                <Accordion.Toggle className="stats" as={Card.Header} eventKey= "1"     onClick={() => setOpen(!open)}>
+                ===StatS+++ {open ? <i className="fas fa-chevron-down"></i> : <i className="fas fa-chevron-up"></i>}
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey= "1">
+                    <Card.Body className="cardBody" bg-transparent>
+                        <p>Height: {characterData.height}</p>
+                        <p>Hair Color: {characterData.hair_color}</p>
+                        <p>Skin Color: {characterData.skin_color}</p>
+                        <p>Eye Color: {characterData.eye_color}</p>
+                        <p>Birth Year: {characterData.birth_year}</p>
+                        <p>Gender: {characterData.gender}</p>
+                        </Card.Body>
+                </Accordion.Collapse>
+         </Card>
+     </Accordion>
+     </Container >
+ )
 }
 export default Character
 
