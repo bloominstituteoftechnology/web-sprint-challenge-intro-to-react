b9ee51e234551a60aee118255b6a733e5ca7e536
Elijah <elijahoneal@yahoo.com>
2020-11-21 23:33:26 -0500

    Character component render to virtual DOM

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 import './App.css';
-
+import Character from "../src/components/Character"
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -8,10 +9,26 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [state , setState] = useState({});
+  useEffect( ()=> {
+  
+    axios.get('https://rickandmortyapi.com/api/character')
+    .then(res => {
+        setState(res.data.results)
+      
+      })
+    .catch( err => err);
+    },[]) 
+   
+
+let charProp = Array.from(state);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {charProp.map((character) => 
+        (<Character character = {character} />))
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,26 @@
 // Write your Character component here
+import React from 'react';
+
+const Character = ( props ) => {
+    console.log(props)
+    
+        return (
+            <div className = 'CharacterCard'>
+            <h3 className = 'title' >{props.character.name}</h3>
+            <img alt = 'rick and morty character' src = { props.character.image }></img>
+            <li className = 'gender' >Gender: { props.character.gender }</li>
+            <li className = 'species'>Species: { props.character.species }</li>
+            <li className = 'location'>Location: { props.character.location.name }</li>
+            <li className = 'origin'>Origin: { props.character.origin.name }</li>
+            <li className = 'status'>Status: { props.character.status }</li>
+            </div>
+        )
+
+
+  }
+export default Character
+
+
+
+
+
