2602853ee785c5e0bfd8c3c68fd8b18611c5917b
loleatha martin <70002094+Loleatha@users.noreply.github.com>
2020-11-23 17:40:44 -0500

    Update Character.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,40 +3,40 @@ import React from `react`;
 import styled from `styled-components`;
 
 
-const StyleCard = styled.div`
-border:
-magrin:
-width:
-font-size:
-color:
-padding:
+const Card = styled.div`
+border: black 2px dashed;
+magrin: 3%;
+width: auto;
+font-size: 6px;
+color: green;
+padding: 10px;
 `
 const StyledInfo = styled.h2 `
-font-size:
-color:
+font-size: 5px;
+color: blue;
 `
 
 const StyleData = styled.p`
-text-align:
+text-align: center;
 `
 
 const Character = props => {
-    const { characterInfo} = props;
+    return (
+        <Card>
+
+    {props.characterData.map(character => {
+        return(
+            <StyledDiv key={character.id}>
+            <h2>Name: {character.name}</h2>,
+            <p>Species: {character.species}</p>,
+            <p>Location: {character.location}</p>,
+            <p>First seen in: {character.episode}</p>,
+                
+            <StyledImg src={character.image} alt={character.name} />
+            </StyledDiv>
+        )
+    })}
+    )
 }
-
-console.log(characterInfo);
-
-return (
-    <StyleCard>
-        <img src={props.character.image} alt={props.character.name} />
-        <h2>{props.character.name}</h2>
-        <p>{props.character.species}</p>
-        <p>{props.character.location}</p>
-        <StyleDeadcard>{props.character.status}</StyleDeadcard>
-        :
-<p>{props.character.status}</p>
-    </StyleCard>
-)
-
 
 export default Character;
