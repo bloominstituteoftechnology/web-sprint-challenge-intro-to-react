5b7c11672fa9dd5d5259f74a5e412c773ccc1861
Valeriusdev <kakao757@gmail.com>
2020-11-22 23:28:42 -0800

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,19 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+const [data, setData] = useState ([]);
+useEffect(() =>{
+  axios
+  .get('https://rickandmortyapi.com/api/character')
+  .then(res => {    
+    setData(res.data.results);
+  });
+},[] );
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character mydata = {data}/>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,28 @@
 // Write your Character component here
+
+import React from 'react';
+import {Card, CardTitle, CardImg, Container, Row, Col, CardSubtitle} from 'reactstrap';
+
+function Character(props){    
+    return(
+        <Container>
+            <Row>
+            {props.mydata.map((element) => {
+               return(                   
+                   <Col sm ='2'>
+                   <Card key = {element.id}>
+                        <CardImg width="80%" src = {element.image} alt = "Image is not available"/>
+                        <CardTitle tag="h4">{element.name}</CardTitle>
+                        <CardSubtitle tag="h5"> Species: {element.species} </CardSubtitle>
+                        <CardSubtitle tag="h5"> Location: {element.location.name} </CardSubtitle>
+                        <CardSubtitle tag="h6">{element.status}</CardSubtitle>                        
+                    </Card>
+                   </Col>                   
+                );
+            })};
+            </Row>            
+        </Container>
+    );
+};
+
+export default Character;
