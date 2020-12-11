b9341a9fd85cbc0481652459137a3016748fde16
Anthony Cortese <akcortese@Anthonys-MacBook-Pro.local>
2020-12-11 10:43:41 -0700

    completed lots of steps, still trying to get it to render

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios"
+import Character from "./components/Character"
 
 const App = () => {
+  const [names, setNames] = useState([]);
+  const [characterId, setCharacterId] = useState("")
+
+  const openDetails = id => {
+    setCharacterId(id)
+  }
+
+  const closeDetails = () => {
+    setCharacterId(null)
+  }
+
+  useEffect(() => {
+    const getChar = () => {
+      axios.get(`https://swapi.dev/`)
+      .then(res => {
+        setNames(res.data);
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+    getChar()
+  }, []);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -11,7 +37,16 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      {
+        names.map(na => {
+          return <Character bold key={na.id} info={na} action={openDetails} />
+        })
+      }
+      {
+        <Character charId={characterId} close={closeDetails} />
+      }
+
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,35 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+import axios from "axios"
+
+export default function Character(props) {
+    const { charID, close } = props;
+    const [ details, setDetails] = useState(null);
+
+    useEffect(() => {
+        axios.get(`https://swapi.dev/`)
+          .then(res => { 
+              setDetails(res.data) 
+            })
+          .catch(err => { 
+              console.log(err) 
+            })
+      }, [charID]);
+
+    return (
+        <div className= "container-fluid">
+            <div className>
+                <h3>Name: {details.name}</h3>
+                <p>Gender: {details.gender}</p>
+                <p>Birth Year: {details.birth_year}</p>
+                <p>Skin Color: {details.skin_color}</p>
+                <p>Eye Color: {details.eye_color}</p>
+                <p>Hair Color: {details.hair_color}</p>
+
+                
+            <button onClick={close}>Close</button>
+            
+            </div>
+        </div>
+    )
+}
