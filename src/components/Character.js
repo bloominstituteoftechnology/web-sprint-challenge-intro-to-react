93977d30114e4338832f9d84b210762a61d65c9f
bigbootygoblins <55123268+bigbootygoblins@users.noreply.github.com>
2020-12-11 10:50:48 -0800

    work in progress, continuing to make things work

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,25 @@
 // Write your Character component here
+//this is where the character information will be passed in. TODO: export the information into the List in App.js
+import React, { useState, useEffect } from "react"
+import App from './App'
+import Axios from  "axios"
+
+Axios.get(https://swapi.dev/api/people/)
+
+const List = () => {
+
+
+    return (
+      <div className="List">
+        <span>
+          <h1>list go here</h1>
+        </span>
+  
+      </div>
+        
+    )
+    }
+
+export default {
+    List
+}
