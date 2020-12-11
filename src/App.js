0eaecf113e929ba16acbe35139acf4c20e571bf4
themusicalshaman <ryanshipley27@gmail.com>
2020-12-11 10:47:24 -0700

    Have created the promise and am able to access data in the components section but struggling to display them properly on the page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import axios from 'axios'
-import Character from './components/Character'
+import axios from 'axios';
+import Character from './components/Character';
+import styled from "styled-components";
 
 
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [characterData, setCharacterData] = useState([])
+  const [character, setCharacter] = useState([])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -16,7 +17,7 @@ const App = () => {
   useEffect(() => {
     axios.get('https://swapi.dev/api/people/')
       .then((res) => {
-        setCharacterData(res.data.results)
+        setCharacter(res.data.results)
       })
       .catch((err) => {
         console.log('Use the force!')
@@ -26,6 +27,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div className='characterCard'>
+        <Character data={character}/>
+      </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,19 +1,31 @@
 // Write your Character component here
 import React from 'react';
+import styled from 'styled-components';
 
 
 
-// const Character = {props} => {
-//     const { data } = props
-//     return (
-//         <CharacterSW>
-//             {data.map((character) => {
-//                 return( 
-//                     <CharCard>
-//                         <h1>Name: {character.name}</h1>
-//                     </CharCard>
-//                 )
-//             })}
-//         </CharacterSW>
+const Character = ({character}) => {
+    const { data } = character
+    return (
+        <CharacterSW>
+            {data.map((character) => {
+                return( 
+                    <CharCard>
+                        <h1>Name: {character.name}</h1>
+                    </CharCard>
+                )
+            })}
+        </CharacterSW>
+    )
+}
+
+// const Character = ({ character }) => {
+//     return(
+//         <Card>
+//             <h2>Name: {character.name}</h2>
+//         </Card>
 //     )
 // }
+
+export default Character
+
