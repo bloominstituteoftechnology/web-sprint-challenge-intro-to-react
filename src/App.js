a4661f1d6b8505f6d0c9a46390fbef238dddedb7
justinalize <justinalize@gmail.com>
2020-12-11 13:39:55 -0500

    mvp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Character from './components/Character';
+import styled from 'styled-components'
 import './App.css';
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+const App = () => {
+  const [characterInfo, setCharacterInfo] = useState([]);
+  
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then(response => {
+      setCharacterInfo(response.data.results);
+    })
+    .catch((error) => {
+      console.log(error);
+      debugger
+    })
+  }, [])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1>CHARACTERS</h1>
+      {
+        characterInfo.map(character => {
+          return <Character key={character.id} data={character} />
+        })
+      }
     </div>
   );
 }
-
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,43 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components'
+
+
+const StyledDiv = styled.div`
+  padding: 0;
+  margin: 0 auto;
+  
+  justify-content: center;
+  font-size:16px;
+  color:white;
+  text-align:center;
+  border:1px solid rgba(57, 255, 20, 0.5);
+  width:50%;
+  flex-wrap: wrap;
+  height:auto;
+  h2 {
+      font-weight:bold;
+      color: white;
+     text-align:center
+      width:50%;
+      height:auto;
+  }
+  p{
+      
+      color: #54ad7a;
+      display:flex;
+      font-size:18px;
+      justify-content:center;
+      
+  }
+`
+
+export default function Character({data}) {
+
+  return (
+    <StyledDiv>      
+    <h2> {data.name} </h2> 
+     <p>DOB: {data.birth_year}</p>
+    </StyledDiv>
+  )
+}
