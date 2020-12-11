3607eacb794daac243c93c0b588c77c0e147e24d
Beau Jennings <redalthazar@gmail.com>
2020-12-11 12:04:07 -0600

    base app scaffolding created, find issue in character.js to resolve undefined issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from "axios";
+import CharacterData from "./components/Character";
+
+
+
 
 const App = () => {
+  const [rmData, setRmData] = useState([]);
+    useEffect(() => {
+      const fetchRmData = () => {
+        axios.get("https://rickandmortyapi.com/api/character")
+        .then(res => {
+          setRmData(res.data);
+        })
+        .catch((err) => {
+          console.log (err);
+        })
+      }
+      fetchRmData()
+    }, [])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +30,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <CharacterData rmData = {rmData}/>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,26 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+export default function CharacterData(props) {
+    const {rmData} = props;
+    
+    return (
+        <styledCharacter>
+            <div>
+                <img src = {rmData.image} alt = "character portrait"/>
+            </div>
+            <h2>{rmData.name}</h2>
+            <div>
+                <h3>Species: {rmData.species}</h3>
+                <h3>Gender: {rmData.gender}</h3>
+                <h3>Status:{rmData.status}</h3>
+                <h3>Origin: {rmData.origin.name}</h3>
+            </div>
+        </styledCharacter>
+    )
+}
+
+const styledCharacter = styled.div`
+    display: flex'
+`
