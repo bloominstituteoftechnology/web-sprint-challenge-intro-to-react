9e3784eb24a7d092b4612705b81293ee8dc0cbae
themusicalshaman <ryanshipley27@gmail.com>
2020-12-11 09:56:56 -0700

    struggled with axios issue, had to install from terminal

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,19 @@
 // Write your Character component here
+import React from 'react';
+
+
+
+// const Character = {props} => {
+//     const { data } = props
+//     return (
+//         <CharacterSW>
+//             {data.map((character) => {
+//                 return( 
+//                     <CharCard>
+//                         <h1>Name: {character.name}</h1>
+//                     </CharCard>
+//                 )
+//             })}
+//         </CharacterSW>
+//     )
+// }
