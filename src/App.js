bd950efd341027fc732929917216dbd6112ef8d4
Lance Sanchez <lancesanchez@outlook.com>
2020-12-11 13:31:06 -0500

    incomplet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Character from "./components/Character";
+import styled from "styled-components";
+
+
 import './App.css';
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [characters, setCharacters] = useState({});
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character')
+    .then(res => {
+      setCharacters(res.data.results)
+    })
+    },[])
 
   return (
     <div className="App">
