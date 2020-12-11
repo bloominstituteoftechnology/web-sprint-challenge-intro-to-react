f9d91591b811e5ed80905ab5f90ce28779aeed45
Brennen O'Connor <brennenoconnor@Brennens-MacBook-Pro.local>
2020-12-11 13:23:57 -0500

    sprint complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ThemeProvider } from 'styled-components'
+import styled from 'styled-components';
+import axios from 'axios';
+import Character from './components/Character'
 import './App.css';
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+const StlyedApp = styled.div`
+  text-align: center;
+  font-size: 50px;
+  h1 {
+    color: white;
+  }
+  font-family: 'Roboto', sans-serif;
+`;
+
+
+const App = () => {
+  const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
+
+
+  useEffect(() => {
+    axios.get(`https://rickandmortyapi.com/api/character/`)
+      .then(res => {
+        setCharacters(res.data.results);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+  }, [page])
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+   
+      <StlyedApp>
+        <div className="App">
+          <h1>Characters</h1>
+          {characters.map(char => {
+            return <Character key={char.id} character={char} />
+          })}
+        </div>
+       
+      </StlyedApp>
+ 
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,54 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components';
+
+
+const StlyedCharacter = styled.div`
+  border: 1px solid black;
+  margin: 2% auto;
+  padding: 2%;
+  h3 {
+    width: 100%;
+    color: black;
+    font-size: 45px;
+    margin: 1rem 0;
+    text-align: center;
+  }
+  p {
+    font-size: 20px;
+    margin: 0.5rem 0;
+  }
+   img {
+    height: 325px;
+    transform: scale(1);
+    transition: .3s ease-out;
+   }
+  transition: background-color 0.3s ease-in;
+  width: 400px;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  justify-content: center;
+  justify-items: center;
+  align-content: center;
+  &:hover {
+    background-color: #A9D3E9;
+    transition: background-color 0.3s ease-out;
+  }
+  `;
+
+
+function Character(props) {
+  const { character } = props
+  return (
+    <StlyedCharacter>
+      <h3>{character.name}</h3>
+      <p><img src={character.image} alt="Character Image" /></p>
+      <p>Species: {character.species}</p>
+      <p>Origin: {character.origin.name}</p>
+    </StlyedCharacter>
+
+  )
+}
+
+export default Character
