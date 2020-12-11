f1f12d2fe0dde0bd30f7deface25d360d1f8a952
Diego Roman <diegormnv209@gmail.com>
2020-12-11 08:56:00 -0800

    Data displaying and some styling done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [charaters, setChararcters] = useState([]);
+
+  useEffect(() =>{
+    axios.get('https://swapi.dev/api/people/')
+    .then((res) =>{
+      // console.log(res.data.results);
+      setChararcters(res.data.results);
+    })
+    .catch((err) =>{
+      console.log(err, 'error')
+    })
+  }, [])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -12,6 +26,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {charaters && charaters.map(character =>{
+        return <Character key={charaters.id} details={character} />
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,28 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Character({details}){
+    
+    return(
+        <div>
+            <StyleCharacter>
+                <p>{details.name}</p>
+                <p>{details.birth_year}</p>
+            </StyleCharacter>
+        </div>
+        
+    )
+}
+
+//styling component
+const StyleCharacter = styled.div`
+    color: white;
+    background-Color: black;
+    width: 70vw;
+    border: 2px solid green;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 1% auto;
+`;
