5492db0dde5298e7576d0a34ab84ae298c9529e0
shawn2776 <=>
2020-12-11 11:14:59 -0800

    not finished, but not stuck

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import NavBar from './components/NavBar';
+import Character from './components/Character'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [ characterOne, setCharacterOne ] = useState([]);
+  const [ currentCharacter, setCurrentCharacter ] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const openDetails = (id) => {
+    setCurrentCharacter(id)
+  }
+
+  const closeDetails = () => {
+    setCurrentCharacter(null)
+  }
+
+  useEffect(() => {
+    axios
+    .get('https://swapi.dev/api/people/')
+    .then((res) => {
+      setCharacterOne(res.data.results[0])
+      
+    })
+    .catch((error) => {
+      alert(error)
+    })
+  }, [])
+
+  
 
   return (
     <div className="App">
+      <NavBar />
       <h1 className="Header">Characters</h1>
+      <Character name={characterOne.name} height={characterOne.height} birthYear={characterOne.birth_year} homePlanet={characterOne.homeworld}/>
     </div>
   );
 }
 
 export default App;
+
+
+ // Try to think through what state you'll need for this app before starting. Then build out
+  // the state properties here.
+
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // side effect in a component, you want to think about which state and/or props it should
+  // sync up with, if any.
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,40 @@
 // Write your Character component here
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const Character = ({ name, height, birthYear, homePlanet }) => {
+  const styleProp = {style: "padding-top: 60px"}
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <p>
+            <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+            {name}
+            </a> 
+          </p>
+          <div class="collapse" id="collapseExample">
+            <div class="card card-body">
+             Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <p>
+            <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample1">
+            {name}
+            </a> 
+          </p>
+          <div class="collapse" id="collapseExample1">
+            <div class="card card-body">
+             Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Character;
