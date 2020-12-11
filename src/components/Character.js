b51aba3373726d91869875dc77d084dd3878dbaf
Corie Stewart <coriestewart1989>
2020-12-11 12:34:22 -0500

    still need to style

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,18 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+
+export default function createCharacter({eachCharacter}){
+    return (
+        <div>
+    <h1 className = 'Name'>{eachCharacter.name}</h1>    
+    <img src = {eachCharacter.image} alt=''/>
+    <p className = 'Status'>{eachCharacter.status}</p>
+    <p className = 'Species'>{eachCharacter.species}</p>
+        </div>
+    
+    
+    )
+    
+}
