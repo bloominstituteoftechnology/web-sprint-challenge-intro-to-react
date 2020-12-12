786d864502569eeaee1ca57b2ef5ac7784a2b311
Andrew Sohrabi <andrewsohrabi@Andrews-MacBook-Pro.local>
2020-12-11 17:58:40 -0800

    starting again from scratch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import './App.css';
+
+import { BASE_URL} from './components/index';
+
+import axios from 'axios'
+import Details from './components/Details'
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,6 +14,28 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [characters, setcharacters] = useState([])
+  const [currentCharacterId, setCurrentCharacterId] = useState('1')
+
+  const openDetails = id => {
+    setCurrentCharacterId(id)
+  }
+
+  const closeDetails = () => {
+    setCurrentCharacterId(null)
+  }
+
+  useEffect(() => {
+    axios.get(`https://rickandmortyapi.com/api/character`)
+      .then(res => {
+        console.log(res.data)
+        setcharacters(res.data.results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
 
   return (
     <div className="App">
---
+++ src/components/Details.js
@@ -0,0 +1,1 @@
+// add details here
