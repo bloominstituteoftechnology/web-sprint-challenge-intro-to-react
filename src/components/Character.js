3d3cd050916eb1b0ed111ede67d2c5a578bfd2e4
MaurooRen <maurooren@gmail.com>
2020-12-11 23:30:28 -0500

    setting Body component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,29 +1,15 @@
 import React from 'react'
 import Image from './Image'
+import Body from './Body'
 import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
 
 
 const Character = ({ data }) => {
-
-    const colorConditional = data.status !== 'unknown' ? data.status === 'Alive' ? 'success' : 'danger' : 'secondary';
-
     return (
         <Card bg="dark" border="dark" text='light'>
-        <Image alt={ data.name } src={ data.image } />
-        <Card.Body>
-            <Card.Title>
-                { data.name } {" "}
-                <Badge pill variant={colorConditional}>{ data.status }</Badge>
-            </Card.Title>
-            <Card.Subtitle className="mb-2">
-                { data.type ? data.type : data.species }
-            </Card.Subtitle>
-            <Card.Text>Last known location:</Card.Text>
-            <Card.Link href="#">{ data.location.name }</Card.Link>
-            <Card.Text>First seen in:</Card.Text>
-            <Card.Link href="#">{ data.origin.name }</Card.Link>
-        </Card.Body>
+            <Image alt={ data.name } src={ data.image } />
+            <Body data={data} />
         </Card>
     )
 }
