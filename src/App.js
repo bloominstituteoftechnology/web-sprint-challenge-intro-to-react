0e5cb0157c82ed9b2ee7079d362a9db392477d92
Lance Sanchez <lancesanchez@outlook.com>
2020-12-12 22:00:17 -0500

    updating

--- src/App.js
+++ src/App.js
@@ -1,33 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Character from "./components/Character";
-import styled from "styled-components";
-
 
 import './App.css';
 
-
-
-const App = () => {
+export default function App() {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [characters, setCharacters] = useState({});
+  const [character, setCharacters] = useState([]);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   useEffect(() => {
-    axios.get('https://rickandmortyapi.com/api/character')
-    .then(res => {
-      setCharacters(res.data.results)
+    const obtChar = () => {
+      axios.get('https://rickandmortyapi.com/api/character')
+      .then(res => {
+        console.log(res.data);
+        setCharacters(res.data)
+      })
+      .catch((err) => {
+        console.log("Loading")
+      })
+    }
+    obtChar()
+  }, [])
+
+//   return (
+//     <div className="App">
+//       <h1 className="Header">Characters</h1>
+//       {character.map(item => 
+//         <Character eachCharacter={item} key={item.id}/>
+//       )
+//       })}
+//     </div>
+//   );
+// }
+
+
+
+// another way to export is declaring inline in font of function
+//export default cont App = () => {}
+
+
+// return (
+//   <div className=“App”>
+//     <div className=“Header”>Characters</div>
+//     {character.map(item  =>
+//           <Character eachCharacter={item} key = {item.id}/>
+//     )}
+//   </div>
+// );
+
+return (
+  <div className="App">
+    <h1 className="Header">Characters</h1>
+    {character.map((fn) => {
+      return <Character key={fn.id} character={fn}/>
+      
     })
-    },[])
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
-}
-
-export default App;
+    }
+  </div>	    
+);	 
+}	
