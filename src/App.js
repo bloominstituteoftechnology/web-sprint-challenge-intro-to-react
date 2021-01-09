5d440c3e736daba642ec19ada1b86e9f396501c0
Rhiannon Stanford <rhiannonstanford@gmail.com>
2021-01-08 20:01:02 -0800

    API successful. Create Character component complete. Props pass to Character successfully.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Character from './components/Character.js';
+
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [data, setData] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(() => {
+
+    axios
+      .get("https://swapi.dev/api/people/")
+      // Which we then set to state
+      .then(res => {
+        setData(res.data.results);
+      console.log("fetch data", res.data.results);
+      })
+      // Always include error handling
+      .catch(err => console.log(err));
+      
+  }, []);
+
+  // useEffect(() => {
+  //   console.log("Log this when data state changes");
+  // }, data);
+
+  // console.log("Data: ", data); 
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {/* <Character data={data}/> */}
+
+      {data.map(char => {
+          return <Character key={char.id} data={char}/>
+        })}
+
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,19 @@
-// Write your Character component here
+import React from 'react';
+import '../App.css';
+
+
+export default function Character({data}) {
+  const { name, height, mass, hair_color, skin_color, eye_color, birth_year, gender } = data;
+  console.log("data in Character: ", data);
+
+  console.log("Name: ", name);
+
+  return (
+    <div className="character-container">
+
+    {name}
+
+
+    </div>
+  );
+}
