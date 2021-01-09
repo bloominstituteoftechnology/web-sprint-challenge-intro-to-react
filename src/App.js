70c2f97d1992c043cd3c6258871d463aa0418621
Molly Brown <mollybrown0516@gmail.com>
2021-01-09 15:32:01 -0500

    Rendered the characters name & birth year to the page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
+import Character from './components/Character';
 
 const App = () => {
-  const [characterData, setCharacterData] = useState({});
+  const [characterData, setCharacterData] = useState([]);
   
   useEffect(() => {
     axios
       .get(`https://swapi.dev/api/people/`)
       .then(res => {
-        setCharacterData(res.data);
-        console.log("fetch data: ", res.data); //We're setting the resolution to data, because that is the property you want from the API that will give me the list of characters.
+        setCharacterData(res.data.results);
+        console.log("fetch data: ", res.data.results); //We're setting the resolution to data, because that is the property you want from the API that will give me the list of characters.
         //Then you will set the res.data to setCharacterData, setting the data to state so we can use it.
       })
       .catch(err => console.log(err));
@@ -25,6 +26,13 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+
+      {characterData.map(character => {
+        
+        return <Character name={character.name} birthYear={character.birth_year}/>
+      }) 
+        
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,19 @@
-// Write your Character component here
+import React from 'react';
+
+export default function Character(props){
+    const { name, birthYear } = props;
+    debugger
+    
+
+    return (
+        <div className='character-card'>
+           <div className='individual-character'>
+                <p>{name} - {birthYear}</p>
+           </div>
+            
+        </div>
+
+    );
+}
+
+
