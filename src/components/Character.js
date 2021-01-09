a07a42135c451dfaa03b3733e5f5050e1f936d1c
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-01-09 18:39:52 -0500

    Character component set up

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,27 @@
 // Write your Character component here
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import StarWarsCard from "./StarWarsCard";
+
+const Character = () => {
+  const [character, setCharacter] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((res) => {
+        console.log(res);
+        setCharacter(res.data.results);
+      })
+      .catch((err) => console.log("err", err));
+  }, []);
+
+  return (
+    <div>
+      {character.map((data) => (
+        <StarWarsCard key={data.name} data={data} />
+      ))}
+    </div>
+  );
+};
+
+export default Character;
---
+++ src/components/StarWarsCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const StarWarsCard = () => {
+
+  return (
+    <div>Star Wars Card</div>
+  )
+}
+
+export default StarWarsCard
