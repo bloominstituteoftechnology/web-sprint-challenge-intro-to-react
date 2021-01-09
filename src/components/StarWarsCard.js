b310217b894547e20d35ccf36c2a061db0c99304
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-01-09 18:49:18 -0500

    StarWarsCard created and working

--- src/components/StarWarsCard.js
+++ src/components/StarWarsCard.js
@@ -1,10 +1,25 @@
 import React from "react";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Col,
+} from "reactstrap";
 
-const StarWarsCard = () => {
-
+const StarWarsCard = (props) => {
   return (
-    <div>Star Wars Card</div>
-  )
-}
+    <Col xs="6" md="4" xl="3">
+      <Card>
+        <CardBody>
+          <CardTitle>NAME: {props.data.name}</CardTitle>
+          <CardSubtitle>CREATED: {props.data.created}</CardSubtitle>
+          <CardText> SPECIES: {props.data.species} </CardText>
+        </CardBody>
+      </Card>
+    </Col>
+  );
+};
 
-export default StarWarsCard
+export default StarWarsCard;
