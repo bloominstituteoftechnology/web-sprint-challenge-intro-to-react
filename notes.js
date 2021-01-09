ac09ce3e90f991844bb69ced982b0d1d0ed73747
Molly Brown <mollybrown0516@gmail.com>
2021-01-09 14:31:53 -0500

    Completed fetching the API data & setting it to state

---
+++ notes.js
@@ -0,0 +1,22 @@
+{name: "Luke Skywalker", height: "172", mass: "77", hair_color: "blond", skin_color: "fair", …}
+birth_year: "19BBY"
+created: "2014-12-09T13:50:51.644000Z"
+edited: "2014-12-20T21:17:56.891000Z"
+eye_color: "blue"
+films: (4) ["http://swapi.dev/api/films/1/", "http://swapi.dev/api/films/2/", "http://swapi.dev/api/films/3/", "http://swapi.dev/api/films/6/"]
+gender: "male"
+hair_color: "blond"
+height: "172"
+homeworld: "http://swapi.dev/api/planets/1/"
+mass: "77"
+name: "Luke Skywalker"
+skin_color: "fair"
+species: []
+starships: (2) ["http://swapi.dev/api/starships/12/", "http://swapi.dev/api/starships/22/"]
+url: "http://swapi.dev/api/people/1/"
+vehicles: (2) ["http://swapi.dev/api/vehicles/14/", "http://swapi.dev/api/vehicles/30/"]
+__proto__: Object
+
+
+
+//Above is my data
--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [characterData, setCharacterData] = useState({});
+  
+  useEffect(() => {
+    axios
+      .get(`https://swapi.dev/api/people/`)
+      .then(res => {
+        setCharacterData(res.data);
+        console.log("fetch data: ", res.data); //We're setting the resolution to data, because that is the property you want from the API that will give me the list of characters.
+        //Then you will set the res.data to setCharacterData, setting the data to state so we can use it.
+      })
+      .catch(err => console.log(err));
+      
+  }, []); 
+  console.log("Data: ", characterData);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
