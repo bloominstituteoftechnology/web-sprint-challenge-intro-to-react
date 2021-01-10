5c8dfa4d29b20bb5fdc5e17cc3270738b9c3235b
daniel-vazquez <da.vazquez728@gmail.com>
2021-01-10 12:48:16 -0700

    imported character data from axios, created basic styling in character.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,12 +10,32 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  
+  const [characters, setCharacter] = useState([]) 
+  
+  useEffect( () => {
+    axios   
+        .get('https://rickandmortyapi.com/api/character')
+        .then(success => {
+            console.log('success', success);
+            setCharacter(success.data.results);
+            
+        })
+
+        .catch(fail => console.log('get fail', fail));
+
+    },[])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map((char) => (
+      <Character key={char.id} character={char} />
+      ))}
     </div>
-  );
-}
+  )}
+
+  
+      
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,41 @@
 // Write your Character component here
+import React, {useState, useEffect} from 'react'
+import styled from 'styled-components'
+
+
+
+const StyledChar = styled.div`
+
+    margin: 10px;
+    padding: 5px;
+    border: 2px solid black;
+    width: 500px;
+
+    img {
+    width: 300px;
+    height: 300px;
+    }
+
+    display: flex;
+    justify-content: center;
+
+
+
+`
+
+const Character = (props) => {
+
+    return (
+        <StyledChar className='container'>
+            <h2>{props.character.name}</h2>
+            <p>Species: {props.character.species}</p>
+            <p>Origin: {props.character.name}</p>
+            <img src={props.character.image}/>
+
+
+        </StyledChar>
+
+    )
+}
+
+export default Character
