ebea55134841e49f4b97aaaa4fe629b68a23e4d5
Rex-1031 <64785141+Rex-1031@users.noreply.github.com>
2021-01-09 21:19:13 -0500

    character data fetched from api via effect hook, applied to card data

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [characters, setCharacters] = useState([]);
+  console.log(characters[0]);;
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   useEffect(() =>{
-    axios.get()
+    axios.get('https://rickandmortyapi.com/api/character/')
       .then(res =>{
         setCharacters(res.data.results)
-        console.log(`Character Results:`,res);
+        console.log(res);
       })
       .catch(err =>{
         console.log('error', err)
@@ -23,10 +24,12 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      <Character />
+      {characters.map(character =>(
+      <Character key = {character.id} name ={character.name} species ={character.species} image ={character.image} origin ={character.origin.name}
+      status ={character.status}/>))}
     </div>
   );
-  console.log(characters[0]);
+  
 }
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -5,15 +5,15 @@ import React from 'react';
 const Character = props =>{
     return(
         <div>
-            <img alt="Character Avatar" />
+            <img src={props.image} alt="Character Avatar" />
             <div>
-                <h4>Charcater Name</h4>
+                <h4>{props.name}</h4>
                 <h5>Species</h5>
-                <p>Character Species</p>
+                <p>{props.species}</p>
                 <h5>Origin:</h5>
-                <p>Character Origin</p>
+                <p>{props.origin}</p>
                 <h5>Status:</h5>
-                <p>Character Status</p>
+                <p>{props.status}</p>
             </div>
         </div>
     )
