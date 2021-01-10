dc9c4eec76f9618342badaac7ce51a2983826314
Rex-1031 <64785141+Rex-1031@users.noreply.github.com>
2021-01-09 21:01:26 -0500

    project character cards started, state and effect hooks set up.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [characters, setCharacters] = useState([]);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  useEffect(() =>{
+    axios.get()
+      .then(res =>{
+        setCharacters(res.data.results)
+        console.log(`Character Results:`,res);
+      })
+      .catch(err =>{
+        console.log('error', err)
+      })
+  }, [])
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character />
     </div>
   );
+  console.log(characters[0]);
 }
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,26 @@
 // Write your Character component here
+import React from 'react';
+
+
+const Character = props =>{
+    return(
+        <div>
+            <img alt="Character Avatar" />
+            <div>
+                <h4>Charcater Name</h4>
+                <h5>Species</h5>
+                <p>Character Species</p>
+                <h5>Origin:</h5>
+                <p>Character Origin</p>
+                <h5>Status:</h5>
+                <p>Character Status</p>
+            </div>
+        </div>
+    )
+
+}
+
+
+
+
+export default  Character;
