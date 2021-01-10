79e09dce643a947155acb7794aafe54d4a69e16d
Polly Yospan <porgroovy@gmail.com>
2021-01-10 17:28:51 -0500

    successfully setting toggle function for character details but not sure how to toggle only the character that got click not all of them at once

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const Character = (props) => {
+export function Character(props) {
+  const [mouseOver, setMouseOver] = useState(false);
+  const [toggle, setToggle] = useState(false);
+
+  const toggler = () => {
+    toggle ? setToggle(false) : setToggle(true);
+  };
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
   return props.data.map((Character) => (
-    <h3 className="Character" key={Character.birth_year}>
-      {Character.name}
-    </h3>
+    <div>
+      <h3
+        className="Character"
+        key={Character.birth_year}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        onClick={toggler}
+        style={{ backgroundColor: mouseOver ? "#9e8f7f" : "#ddcdb3" }}
+      >
+        {Character.name}
+      </h3>
+      {toggle && (
+        <div className="Details">
+          <p>Height:{Character.height}</p>
+          <p>Mass: {Character.mass}</p>
+          <p>Hair Color: {Character.hair_color}</p>
+          <p>Skin Color: {Character.skin_color} </p>
+          <p>Eye Color: {Character.eye_color}</p>
+          <p>Birth Year: {Character.birth_year}</p>
+          <p>Gender: {Character.gender}</p>
+        </div>
+      )}
+    </div>
   ));
-};
+}
