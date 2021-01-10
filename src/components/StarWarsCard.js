0a64327638667f2d27bef4246478ecc3944959f2
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-01-09 20:25:56 -0500

    can't get bootstrap to work

--- src/components/StarWarsCard.js
+++ src/components/StarWarsCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import styled from "styled-components";
 import {
   Card,
   CardBody,
@@ -8,17 +9,24 @@ import {
   Col,
 } from "reactstrap";
 
+// const WrapperDiv = styled.div`
+//   font-family: sans-seriff;
+//   text-align: center;
+// `;
+
 const StarWarsCard = (props) => {
   return (
-    <Col xs="6" md="4" xl="3">
-      <Card>
-        <CardBody className="card-text">
-          <CardTitle>NAME: {props.data.name}</CardTitle>
-          <CardSubtitle>CREATED: {props.data.created}</CardSubtitle>
-          <CardText> SPECIES: {props.data.species} </CardText>
-        </CardBody>
-      </Card>
-    </Col>
+    <WrapperDiv>
+      <Col xs="6" md="4" xl="3">
+        <Card>
+          <CardBody className="card-text">
+            <CardTitle>NAME: {props.data.name}</CardTitle>
+            <CardSubtitle>CREATED: {props.data.created}</CardSubtitle>
+            <CardText> SPECIES: {props.data.species} </CardText>
+          </CardBody>
+        </Card>
+      </Col>
+    </WrapperDiv>
   );
 };
 
