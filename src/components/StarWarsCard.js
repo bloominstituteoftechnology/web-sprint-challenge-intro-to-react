acf1851adc666d0db62c03035a58f379f8cfdf40
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-01-09 20:26:35 -0500

    edit

--- src/components/StarWarsCard.js
+++ src/components/StarWarsCard.js
@@ -16,17 +16,15 @@ import {
 
 const StarWarsCard = (props) => {
   return (
-    <WrapperDiv>
-      <Col xs="6" md="4" xl="3">
-        <Card>
-          <CardBody className="card-text">
-            <CardTitle>NAME: {props.data.name}</CardTitle>
-            <CardSubtitle>CREATED: {props.data.created}</CardSubtitle>
-            <CardText> SPECIES: {props.data.species} </CardText>
-          </CardBody>
-        </Card>
-      </Col>
-    </WrapperDiv>
+    <Col xs="6" md="4" xl="3">
+      <Card>
+        <CardBody className="card-text">
+          <CardTitle>NAME: {props.data.name}</CardTitle>
+          <CardSubtitle>CREATED: {props.data.created}</CardSubtitle>
+          <CardText> SPECIES: {props.data.species} </CardText>
+        </CardBody>
+      </Card>
+    </Col>
   );
 };
 
