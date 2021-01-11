89f186578fcba65065ae57e2778b2ebbb922e431
Rhiannon Stanford <rhiannonstanford@gmail.com>
2021-01-11 15:53:48 -0800

    Install custom local hosted font

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import WebFont from 'webfontloader';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// WebFont.load({
+//   local: {
+//     families: ['Distant Galaxy', 'sans-serif']
+//   }
+// });
