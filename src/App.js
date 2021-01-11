d1e3485e35a706139e4dd4e4f45dbbfbf6733d67
samuel singletary <codysingletary92@gmail.com>
2021-01-10 19:15:58 -0500

    made components and put dummyData as to not ping the server continually

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
+import data from './dummyData.js';
+
+
+console.log(data)
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +14,31 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+
+
+  // axios.get('https://swapi.py4e.com/api/people/')
+  //     .then( result => {
+  //         console.log(result)
+  //       })
+  //     .catch( err => console.log(err))  
+
+  
+
+  // while (url !== null){
+  //   axios.get(url)
+  //     .then( result => {
+  //         let {data} = result;
+  //         console.log(data.next)
+  //         url = data.next
+  //       })
+  //     .catch(error => console.log(error))
+  // }
+  
+
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Starwars Characters</h1>
     </div>
   );
 }
