53546d1edca70f3261e364ad63e23b1ce04eb391
BradleyHubbs <75264076+BradleyHubbs@users.noreply.github.com>
2021-01-22 12:46:03 -0500

    overwhelmed, need to go back and practice

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,12 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export default function Pokemon(props) {
+    const {pokemon, setPokemon} = props
+
+    return (
+        <>
+    )
+   
+}
