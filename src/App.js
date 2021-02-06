9603dfc89d1964c8ca2f26771b584147b4776b0d
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-02-05 22:46:12 -0500

    Add styled components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Character from './components/Character';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  min-height: 100vh;
+  text-align: center;
+  display: flex;
+  flex-flow: column nowrap;
+`;
+
+const StyledHeader = styled.h1`
+  color: #fff;
+  flex-basis: 260px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  font-size: 5rem;
+  text-transform: uppercase;
+  letter-spacing: 10px;
+`;
+
+const CharacterListWrapper = styled.div`
+  flex: 1 1 auto;
+  background-image: linear-gradient(rgba(17, 19, 29, 0), rgba(17, 19, 29, 1) 60%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 50px;
+`;
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characterList, setCharacterList] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+const fetchCharacters = () => {
+  axios.get('https://swapi.dev/api/people')
+  .then(response => {console.log(response.data); setCharacterList(response.data)})
+}
+
+useEffect(fetchCharacters, []);
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <StyledApp className="App">
+      <StyledHeader className="Header">Characters</StyledHeader>
+      <CharacterListWrapper>
+        {
+          characterList.map((character, index) => {
+            return <Character character={character} key={index}></Character>
+          })
+        }
+      </CharacterListWrapper>
+    </StyledApp>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,47 @@
-// Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledCharacter = styled.div`
+    background: #2f313e;
+    color: #fff;
+    border: 4px solid #383a45;
+    border-radius: 5px;
+    margin: 10px;
+    padding: 20px;
+    flex: 1;
+    text-align: left;
+    transition: all .2s linear;
+
+    h3 {
+        text-align: center;
+        margin-bottom: 20px;
+    }
+
+    p {
+        line-height: 2;
+    }
+
+    &:hover {
+        cursor: pointer;
+        border: 4px solid #d8763a;
+        transition: all .2s linear;
+    }
+`;
+
+const Character = (props) => {
+    const { character } = props;
+    return (
+        <StyledCharacter>
+            <h3>{character.name}</h3>
+            <p>Birth Year: {character.birth_year}</p>
+            <p>Gender: {character.gender}</p>
+            <p>Height: {character.height}</p>
+            <p>Mass: {character.mass}</p>
+            <p>Eye Color: {character.eye_color}</p>
+            <p>Hair Color: {character.hair_color}</p>
+            <p>Skin Color: {character.skin_color}</p>
+        </StyledCharacter>
+    );
+}
+
+export default Character;
