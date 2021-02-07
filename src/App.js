6928f6e2c9d6f643ea404d34e0d22077a41b202a
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2021-02-07 00:38:16 -0500

    julian petrisky

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React , {useState, useEffect} from 'react';
 import './App.css';
-
+import Character from "./components/Character"
+import axios from "axios"
+import styled from "styled-components"
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+const {characterInfo, setcharacterInfo} = useState([]);
+
+useEffect(() => {
+  axios.get('https://swapi.dev/api/people/')
+
+  .then((res) => {
+    setcharacterInfo(res.data.results);
+  })
+
+  .catch((err) => {
+    console.log(err);
+  })
+}, [])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      <AppStyled>{characterInfo && characterInfo.map(character => {
+
+  return <Character/>
+        })}</AppStyled>
     </div>
   );
 }
+
+const AppStyled = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,64 @@
-// Write your Character component here
+
+import React from "react"
+import styled from "styled-components";
+    const Character  =  (props) => {
+  
+    return (
+        <characterStyle>
+      <div className="character-container">
+        <div className="character-info">
+          <div>
+            <h3>Name: {props.character.name}</h3>
+            <p>BirthYear: {props.character.birth_year}</p>
+            <p>Hair Color: {props.character.hair_color}</p>
+            <p>Height: {props.character.height}</p>
+            <p>Gender: {props.character.gender}</p>
+            <p>Mass: {props.character.mass}</p>
+            <p>Species: {props.character.species}</p>
+          </div>
+        </div>
+      </div></characterStyle>)}
+
+
+const characterStyle = styled.div`
+    display: flex;
+    flex-flow: column nowrap;
+
+    align-items: center;
+    justify-content: center;
+    justify-items: center;
+    align-content: center; 
+
+    border: 5px solid black;
+    background-color: #daeaff;
+
+    width: 1000px;
+
+    margin: 0 auto;
+    margin-bottom: 20px;
+    margin-top: 10px;
+
+    padding: 10px;
+    padding-top: 0px;
+    padding-bottom: 50px;
+    
+    h2 {
+        font-size: 50px;
+        font-weight: bold;
+
+        background-color: black;
+        width: 102%;
+
+        margin-top: 0px;
+        color: #08889a;
+        -webkit-text-stroke: 0.1px white;
+    }
+    p {
+        font-size: 30px;
+        font-family: sans-serif:
+        font-weight: bold;
+
+        margin-top: 0px;
+    }`
+
+    export default Character
