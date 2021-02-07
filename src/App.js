e9d5778eefe433ca96cd4db4fbd2b803069744a8
MaurooRen <maurooren@gmail.com>
2021-02-07 17:37:13 -0500

    axios done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { BASE_URL } from './constants/index';
 import './App.css';
 
 const App = () => {
@@ -8,6 +10,24 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [peopleList, setPeopleList] = useState([])
+
+  const fetchPeople = () => {
+    axios.get(`${BASE_URL}people/`)
+      .then(res => {
+        console.log('the API is running, catch it!')
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log('The API did Ups!')
+      })
+  }
+
+  useEffect(fetchPeople, [])
+  console.log(peopleList)
+
+
 
   return (
     <div className="App">
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,11 @@
-// Write your Character component here
+import React from 'react'
+
+const Character = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Character
