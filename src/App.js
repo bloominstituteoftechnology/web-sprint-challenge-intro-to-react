2ed5597cf5e44dea1e1e57543ba19d75121f3c93
Mark Stahl <markstahl0219@gmail.com>
2021-02-07 19:20:02 -0500

    MVP with styled components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  text-align: center;
+  align-items: center;
+  justify-content: space-around;
+  width: 70%;
+  margin: 0 auto;
+`
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+// Try to think through what state you'll need for this app before starting. Then build out
+// the state properties here.
+const [data, setData] = useState([])
+// Fetch characters from the API in an effect hook. Remember, anytime you have a 
+// side effect in a component, you want to think about which state and/or props it should
+// sync up with, if any.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+useEffect(() => {
+    axios.get("https://swapi.py4e.com/api/people/")
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+    .then((res)=>{
+      console.log(res)
+      setData(res.data.results)
+
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+
+}, [])
+
+return (
+  <div className="App">
+  <h1 className="Header">Star Wars Characters</h1>
+  <StyledApp>
+
+      <Character data = {data} />
+      </StyledApp>
+  </div>
+);
 }
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,37 @@
-// Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const CharacterCards = styled.div`
+      background-color: black;
+      opacity: 80%;
+      color: lightgrey;
+      width: 400px;
+      margin-left: auto;
+      margin-right: auto;
+      border-radius: 2%;
+      margin-bottom: 5%;
+      padding: 4px;
+`
+
+const Character = props => {
+let {data} = props;
+
+    return(
+        <div>
+            {data.map(starWars =>{
+                return (
+                    <CharacterCards>
+            <div>
+            <h3>Name: {starWars.name}</h3>
+            <h3>Gender: {starWars.gender}</h3>
+            <h3>Birth Year: {starWars.birth_year}</h3>
+            </div>
+            </CharacterCards>
+                )
+})}
+
+        </div>
+
+    )
+}
+export default Character;
