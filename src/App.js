c465968bd6758bdc3b92cb6fdd49835044e9448c
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-02-19 13:08:31 -0500

    Got axios call working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Axios from 'axios'
 import './App.css';
+import Character from './components/Character'
+// import constants from './constants'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [starWarsCharacters, setStarWarsCharacters] = useState([])
+  // const [currentCharacterId, setCurrentCharacterId] = useState('')
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect (() => {
+    const fetchCharacterData1 = () => {
+      Axios
+      .get('https://swapi.dev/api/people/?format=json')
+      .then(res => {
+        console.log(`this is working`, res)
+        setStarWarsCharacters(res.data)
+      })
+      .catch(err => {
+        console.log(`${err}`)
+        console.log('U GOT AN ERR')
+    })
+    }
+
+    fetchCharacterData1()
+
+  }, [])
+  
+
+
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {starWarsCharacters.map((ch) => {
+        return <Character key={Math.random()} starWarsCharacters={starWarsCharacters}/>
+      })}
+
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,10 @@
 // Write your Character component here
+import React from 'react'
+
+
+export default function Character(props) {
+    return (
+        <div>
+            <h3>{props.name}</h3>
+        </div>
+      )}
