f6d0747943ad8760636f83a7ae9fa6a48d137103
William Dye <williamdye@williams-mbp.myfiosgateway.com>
2021-02-19 08:59:34 -0500

    Initial

---
+++ build/precache-manifest.4af3841395b649bcf4020e59421b54b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5fdb55aa7888bd816daf04c7ddd25c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6f20e38e316e097ca8c",
+    "url": "/static/css/main.87c00a12.chunk.css"
+  },
+  {
+    "revision": "bdc6008674c7670dc5aa",
+    "url": "/static/js/2.9545494e.chunk.js"
+  },
+  {
+    "revision": "c770aeee4edfa09a108dcfb6a4c523c1",
+    "url": "/static/js/2.9545494e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f20e38e316e097ca8c",
+    "url": "/static/js/main.97123364.chunk.js"
+  },
+  {
+    "revision": "57fca467b55bfed9c5ca",
+    "url": "/static/js/runtime-main.f688ac41.js"
+  },
+  {
+    "revision": "dff75a1ffda792b2380226033f28415c",
+    "url": "/static/media/sw-bg.dff75a1f.jpg"
+  }
+]);
