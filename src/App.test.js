93d3c13dbc556eeedad51ad12dafc141596dcb5f
GregoryPWalton <75237506+GregoryPWalton@users.noreply.github.com>
2021-02-19 16:48:54 -0800

    gregory-walton

--- src/App.test.js
+++ src/App.test.js
@@ -10,7 +10,7 @@ afterEach(() => {
   server.resetHandlers();
   document.body.innerHTML = "";
 });
-
+  
 describe("<App />", () => {
   it("fetches characters from the Star Wars API and displays them", async () => {
     render(<App />);
