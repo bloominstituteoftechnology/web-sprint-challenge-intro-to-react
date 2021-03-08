29d590ae8dd83bba0ed2c0eadc9f4772b2edf353
Kilgharrahcalls <69174493+Kilgharrahcalls@users.noreply.github.com>
2021-03-07 22:33:27 -0500

    NEW

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import CharacterContainer from "./components/Character.js";
+import styled from "styled-components";
 
 const App = () => {
+  const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    const data = axios
+      .get("https://swapi.dev/api/people/?")
+      .then((response) => {
+        console.log(response);
+        setCharacters(response.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log(data);
+  }, []);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -11,9 +27,22 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars</h1>
+
+      {characters.map((character) => {
+        return (
+          <CharacterContainer
+            key={character.id}
+            name={character.name} 
+            mass={character.mass}
+            birthyear={character.birthyear}
+            gender={character.gender}
+          />
+        );
+      })}
     </div>
+    
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,46 @@
+import React from 'react';
+import styled from "styled-components";
 // Write your Character component here
+const character = ( {name,mass,gender,birthyear}) =>{
+    const StyleDiv= styled.div`
+    color: green;
+    font-weight: bold;
+    font-size: 12px;
+    
+    `;
+    
+    const FirstName= styled.div`
+    text-align:center;
+    color: purple;
+    font-weight: bold;
+    border: black 5px dashed;
+    font-size: 20px;
+    `;
+    
+    
+    return (
+        <StyleDiv>
+        <div className = 'Character-Container'>     
+        <FirstName>
+            <h1>{character}</h1>
+            <p>{name}</p>
+            </FirstName>
+            <p>{mass}</p>
+           <p>{birthyear}</p> 
+           <p>{gender}</p> 
+            </div>
+            </StyleDiv>
+           
+           
+    
+            
+    
+    );
+    };
+    
+    
+    
+    
+    
+    export default character;
+    
