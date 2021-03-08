fea6d15714eedc195749ef25feb5fdce5f8d7eb9
Kilgharrahcalls <69174493+Kilgharrahcalls@users.noreply.github.com>
2021-03-07 22:49:17 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
@@ -33,7 +33,7 @@ const App = () => {
         return (
           <CharacterContainer
             key={character.id}
-            name={character.name} 
+            name={character.name}
             mass={character.mass}
             birthyear={character.birthyear}
             gender={character.gender}
@@ -41,7 +41,6 @@ const App = () => {
         );
       })}
     </div>
-    
   );
 };
 
