38c96377eefe3076cb1c242e388fb0ac31e714b2
vida <ofo28@aol.com>
2021-04-05 00:59:57 -0400

    README.md questions completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+//import index from  './index.js'
+import axios from 'axios'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,32 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  
+  const [data, setData] = useState([])
+  
+  useEffect(()=> {
+    axios
+    .get(`https://swapi.dev/api/people/1/`)
+    .then(res => {
+      setData(res.data)
+      console.log(res.data, 'data')
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+     {data.map( => {
+        return <data
+        key={data.id}
+        data={data}
+        setData={setData}
+        />
+      })}
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import styled from 'styled-components'
 import { worker } from "./mocks/browser";
 worker.start();
 
