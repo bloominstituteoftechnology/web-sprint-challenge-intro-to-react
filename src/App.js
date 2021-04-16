3aaf62375a06e770a1cf0267a8aaafb62abac67c
ricardo-pena <ricardo.penagarcia06@gmail.com>
2021-04-16 11:18:04 -0400

    Constructed Character component to pull and display character data on website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character.js'
+// import { rest } from 'msw/lib/types';
 
+const url = "https://swapi.dev/api/people";
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -9,9 +13,19 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+
+  useEffect(()=>{
+    axios.get(url).then((res)=>{
+      console.log("DATA!",res);
+    })
+},[])
+
+
+  
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character src = {url}/>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,49 @@
 // Write your Character component here
+import React, {useState,useEffect} from 'react';
+
+import axios from 'axios';
+import { render } from '@testing-library/react';
+
+
+
+const url = "https://swapi.dev/api/people";
+export default function Characters(props){
+    const {src} = props;
+    const [characters ,setCharacters] = useState([]);
+    useEffect(()=>{
+        axios.get(src).then((res)=>{
+            setCharacters(res.data);
+        //     console.log("CHARACTERS!",res.data);
+        //   console.log("DATA!",res);
+        })
+    },[])
+    
+    const Setup = (props)=>(
+        
+        <div>
+        <div>{props.info.name}</div>
+        <div>
+            <div>{props.info.height}</div>
+            <div>{props.info['hair_color']}</div>
+            <div>{props.info['skin_color']}</div>
+
+
+        </div>
+    </div>
+    );
+      
+    // let done = characters.map((item, i)=>{
+    //     return <Setup key = {i} info = {item}/>
+    // })
+    //   console.log("DONE!",done);
+    
+    // console.log('Info!',characters)
+    
+ return (
+     <div>
+         { characters.map((item, i)=>{
+        return <Setup key = {i} info = {item}/>
+    })}
+     </div>
+ );
+}
