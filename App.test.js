180cfbf8cb3238ded223da69f5fbe281cc5d8a1f
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-04-30 18:19:03 +0200

    tests

--- App.test.js
+++ App.test.js
@@ -12,36 +12,34 @@ afterEach(() => {
 });
 
 describe("<App />", () => {
-  test("Luke Skywalker is _not_ in the DOM on page load", async () => {
+  test("[2] Luke Skywalker eventually appears in the DOM", async () => {
     render(<App />);
     expect(screen.queryByText(/Luke/i)).not.toBeInTheDocument();
+    expect(await screen.findByText(/Luke/i)).toBeInTheDocument();
   });
-  test("C-3PO is _not_ in the DOM on page load", async () => {
+  test("[3] C-3PO eventually appears in the DOM", async () => {
     render(<App />);
     expect(screen.queryByText(/3PO/i)).not.toBeInTheDocument();
+    expect(await screen.findByText(/3PO/i)).toBeInTheDocument();
   });
-  test("R2-D2 is _not_ in the DOM on page load", async () => {
+  test("[4] R2-D2 eventually appears in the DOM", async () => {
     render(<App />);
     expect(screen.queryByText(/R2/i)).not.toBeInTheDocument();
+    expect(await screen.findByText(/R2/i)).toBeInTheDocument();
   });
-  test("Darth Vader is _not_ in the DOM on page load", async () => {
+  test("[5] Darth Vader eventually appears in the DOM", async () => {
     render(<App />);
     expect(screen.queryByText(/Vader/i)).not.toBeInTheDocument();
+    expect(await screen.findByText(/Vader/i)).toBeInTheDocument();
   });
-  test("Leia Organa is _not_ in the DOM on page load", async () => {
+  test("[6] Leia Organa eventually appears in the DOM", async () => {
     render(<App />);
     expect(screen.queryByText(/Leia/i)).not.toBeInTheDocument();
+    expect(await screen.findByText(/Leia/i)).toBeInTheDocument();
   });
-  test("Owen Lars is _not_ in the DOM on page load", async () => {
+  test("[7] Owen Lars eventually appears in the DOM", async () => {
     render(<App />);
     expect(screen.queryByText(/Owen/i)).not.toBeInTheDocument();
-  });
-  test("All characters' names eventually populate in the DOM", async () => {
-    render(<App />);
-    expect(await screen.findByText(/Luke/i)).toBeInTheDocument();
-    expect(await screen.findByText(/3PO/i)).toBeInTheDocument();
-    expect(await screen.findByText(/R2/i)).toBeInTheDocument();
-    expect(await screen.findByText(/Vader/i)).toBeInTheDocument();
-    expect(await screen.findByText(/Leia/i)).toBeInTheDocument();
+    expect(await screen.findByText(/Owen/i)).toBeInTheDocument();
   });
 });
--- setupTests.js
+++ setupTests.js
@@ -2,10 +2,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+
 import "@testing-library/jest-dom/extend-expect";
 
-test("loads and displays greeting", async () => {
-  // Arrange
-  // Act
-  // Assert
-});
+test("[1] The application compiles", () => { });
