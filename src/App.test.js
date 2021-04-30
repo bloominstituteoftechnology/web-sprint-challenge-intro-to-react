5c815965e412ca3c537a63455bfc7ec57f54a0e6
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-04-30 16:34:15 +0200

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -12,19 +12,32 @@ afterEach(() => {
 });
 
 describe("<App />", () => {
-  test("The DOM does not contain the character names on page load", async () => {
+  test("Luke Skywalker is _not_ in the DOM on page load", async () => {
     render(<App />);
-
     expect(screen.queryByText(/Luke/i)).not.toBeInTheDocument();
+  });
+  test("C-3PO is _not_ in the DOM on page load", async () => {
+    render(<App />);
     expect(screen.queryByText(/3PO/i)).not.toBeInTheDocument();
+  });
+  test("R2-D2 is _not_ in the DOM on page load", async () => {
+    render(<App />);
     expect(screen.queryByText(/R2/i)).not.toBeInTheDocument();
+  });
+  test("Darth Vader is _not_ in the DOM on page load", async () => {
+    render(<App />);
     expect(screen.queryByText(/Vader/i)).not.toBeInTheDocument();
+  });
+  test("Leia Organa is _not_ in the DOM on page load", async () => {
+    render(<App />);
     expect(screen.queryByText(/Leia/i)).not.toBeInTheDocument();
   });
-
-  test("The character's names eventually populate in the DOM", async () => {
+  test("Owen Lars is _not_ in the DOM on page load", async () => {
     render(<App />);
-
+    expect(screen.queryByText(/Owen/i)).not.toBeInTheDocument();
+  });
+  test("All characters' names eventually populate in the DOM", async () => {
+    render(<App />);
     expect(await screen.findByText(/Luke/i)).toBeInTheDocument();
     expect(await screen.findByText(/3PO/i)).toBeInTheDocument();
     expect(await screen.findByText(/R2/i)).toBeInTheDocument();
