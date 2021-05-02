384ad4dd3686a0db0c2ba43b5c2db69410e36f49
Owen  Damron <damronowen@yahoo.com>
2021-05-02 14:58:43 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+const [characters, setCharacters] = useState([])
+const [currentCharacterId, setcurrentCharacterId] = useState(null)
 
+const openCharDetails = id => {
+  setCharacterId(id)
+}
+
+const closeCharDetails = id => {
+  setCharacterId(null)
+}
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+useEffect(() => {
+  axios.get('https://swapi.dev/api/people')
+  .then(({data}) => setCharacters(data))
+  .catch((err) => console.log(err))
+}, [])
 
+const Character = props => (
+  <div className='character'>
+    {props.name}
+    <button onClick={() => openCharDetails(props.id)}>
+      Holo-file
+    </button>
+  </div>
+)
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">REACT WARS</h1>
+      {characters.map(characterId => {
+        return <Character key={characterId.id} />
+      })
+      }
+      {
+        currentCharacterId && <Character characterId={currentCharacterId} close={closeCharDetails} />
+      }
     </div>
   );
 }
