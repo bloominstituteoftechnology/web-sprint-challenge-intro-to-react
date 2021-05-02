0cf9a8e7a021788f598a8c94af489e94b096afcf
Jeff <macookpro@MACOOKs-MacBook-Pro.local>
2021-05-02 18:57:24 -0400

    Finish MVP

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Character from './components/Character'
+import axios from 'axios';
 
 const App = () => {
+  const [characterList, setCharacterList] = useState([]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people`)
+    .then(({data}) => {
+      setCharacterList(data);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [])
+
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character props={characterList} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,47 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+function Character(props) {
+    const characterInfo = props.props;
+
+    return (
+        <WrapperDiv>
+            {characterInfo.map(info => {
+                return (
+                    <WrapperDiv>
+                        <Paragraph>{info.name}</Paragraph>
+                        <p>Height: {info.height}</p>
+                        <p>Mass: {info.mass} </p>
+                        <p>Hair color: {info.hair_color}</p>
+                        <p>Skin color: {info.skin_color}</p>
+                        <p>Eye color: {info.eye_color}</p>
+                        <p>Birth year: {info.birth_year}</p>
+                        <p>Gender: {info.gender}</p>
+                    </WrapperDiv>
+                );
+            })}
+        </WrapperDiv>         
+    );
+}
+
+
+
+
+
+export default Character;
+
+
+
+const WrapperDiv = styled.div`
+    width: 100%;
+    height: 100%;
+`;
+
+
+
+
+const Paragraph = styled.p`
+    font-size: 25px;
+    font-weight: bold;
+`;
