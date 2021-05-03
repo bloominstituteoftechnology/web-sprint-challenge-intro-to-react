690a99cda42ed12ea94c1aaf5cd28c57ba808ef3
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-05-03 00:00:21 -0500

    started character component

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = () => {
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-  const name = steve
+  // sync up with, if any. 
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,19 @@
 // Write your Character component here
+import React from 'react'
+import {useState} from 'react'
+import axios from 'axios'
+
+
+export default function Character(props){
+    const [data, useData] = useState(null)
+
+    useEffect(()=>{
+        axios
+        .get('https://swapi.dev/api/people')
+        .then(({data}) => useData(data))
+        .catch(err =>{
+            console.log(err)})
+    },[])
+
+
+}
