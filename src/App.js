6be5b4dd6d05c93f85de0ec8be86d69904e941c3
MarkelWhit <litslayer@gmail.com>
2021-05-14 17:37:12 -0500

    Trying to render character (not working)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { data } from './mocks/handlers'
+import Characters from './components/Character';
+
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+const [char, setChar] = useState(data)
+useEffect()
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Characters</h1>   
+       <Characters character={char} />
     </div>
+
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,12 @@
-// Write your Character component here
+import React from 'react'
+
+
+export default function Characters(props) {
+    const {char} = props;
+    console.log(char)
+    return (
+        <div className='char-container'>
+            <div>{char[0].name}</div>
+        </div>
+    )
+}
