35e1835a1f89ed547c69cbfb3f7284b870d945c7
kazden.jolley <kazden.jolley@gmail.com>
2021-05-14 12:32:29 -0600

    Effect hook and API call working correctly

--- README.md
+++ README.md
@@ -36,7 +36,7 @@ In meeting the minimum viable product (MVP) specifications listed below, your pr
 
 Your finished project must include all of the following requirements:
 
-- [ ] Use the endpoint `[GET] https://swapi.dev/api/people` (mocked in [msw](https://github.com/mswjs/msw)) to obtain characters.
+- [ ] Use the endpoint `[GET] https://swapi.dev/api/people/` (mocked in [msw](https://github.com/mswjs/msw)) to obtain characters.
 - [ ] Set the list of characters into state.
 - [ ] Render your characters to the DOM:
 
--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [charactersInfo, setCharactersInfo] = useState(null);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios
+      .get('https://swapi.dev/api/people/')
+      .then(res => {
+        setCharactersInfo(res.data.results);
+      })
+      .catch(err => {
+        console.log('Please note this error:\n', err);
+      })
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        charactersInfo && 
+        <>
+        </>
+      }
     </div>
   );
 }
