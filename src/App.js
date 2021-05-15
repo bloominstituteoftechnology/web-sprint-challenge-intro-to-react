3d378295104dc1ff19d8888237fab82bb6c035fb
MarkelWhit <litslayer@gmail.com>
2021-05-15 17:17:00 -0500

    Little after hours work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { data } from './mocks/handlers'
+import { data as charData} from './mocks/handlers'
 import Characters from './components/Character';
 
 
@@ -8,8 +8,8 @@ import Characters from './components/Character';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-const [char, setChar] = useState(data)
-useEffect()
+const [char, setChar] = useState(charData)
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -17,7 +17,7 @@ useEffect()
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>   
-       <Characters character={char} />
+       <Characters char={char}/>
     </div>
 
   );
--- src/components/Character.js
+++ src/components/Character.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 
 export default function Characters(props) {
-    const {char} = props;
-    console.log(char)
+const { char } = props;
+console.log(char)
     return (
         <div className='char-container'>
-            <div>{char[0].name}</div>
+            <div className='char-info'>
+                <div>{char[0].name}</div>
+            </div>
         </div>
     )
 }
