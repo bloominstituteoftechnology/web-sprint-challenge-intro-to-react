3fb07ecc007711103e82f32cd7e360eeaade77ef
Amlak befekadu <amlakbarkon@gmail.com>
2021-05-30 01:34:05 -0400

    fetch is working and display one

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+ import Character from './components/Character'
 import './App.css';
-
-const App = () => {
+const App = () => {  
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+const [list, setList] = useState([])
+const url = `https://swapi.dev/api/people/`;
+useEffect(()=>{
+const peopleList =()=>{
+    fetch(url)
+    .then(res=>{
+      return res.json();
+    })
+    .then(res=>{
+      res.map((item,i)=>{
+        return  fetch(url.concat(i+1))
+        .then(res=>{
+          return res.json()
+        })
+        .then(res=>{
+          return setList(res.name)
+        })
+       
+      })
 
+    })
+  } 
+  peopleList();
+  
+},[url])
+    
+  
+   
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+ 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character name={list} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,12 @@
 // Write your Character component here
+
+function Character(props){
+    console.log("THis is props ", props)
+    return (
+        <div>
+          <h1>{props.name}</h1>
+          <p>{props.name}</p>
+        </div>
+    )
+}
+export default Character
