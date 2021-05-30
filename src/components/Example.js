352696855b01ec0deee89cce5afe95083be56d94
Amlak befekadu <amlakbarkon@gmail.com>
2021-05-30 16:13:30 -0400

    done css

--- src/components/Example.js
+++ src/components/Example.js
@@ -10,18 +10,13 @@ list-style-type: none;
 color: white;
 background-color: black;
 font-size: 28px;
+width: 300px;
+transition: width 2s;
 &:hover{
     background-color: rgb(169,141,110);
     color: red;
     font-size: 30px;
     opacity: 1;
-   
+    /* width: 350px; */
 }
-`;
-
-export const P = styled.p`
-  /* color: red;
-  background-color: black;
-  font-size: 18px; */
-  
 `;
