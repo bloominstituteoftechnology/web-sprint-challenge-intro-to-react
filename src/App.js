35a422accf8fa22ae6ce2a537be09fde88c926ed
scoutreilly <scouty33@gmail.com>
2021-05-31 00:03:13 -0600

    not pretty, but working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import Character from './components/Character'
+import axios from 'axios';
+
 import './App.css';
 
 const App = () => {
@@ -8,10 +12,21 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people`)
+    .then((data) => {
+      setCharacters(data.data);
+      // console.log(data.data)
+    })
+    .catch(e => {console.log(e)})
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character props={characters} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
+/* eslint-disable no-lone-blocks */
 // Write your Character component here
+import React from 'react';
+
+function Character(props) {
+  const characterDeets = props.props;
+  console.log(characterDeets);
+  const newList = characterDeets.map(i => {
+    return (
+      <div>
+        <p>My name is {i.name}</p>
+        <p>Check me out</p>
+        <p>Gender: {i.gender}</p>
+        <p>Birth: {i.birth_year}</p>
+        <p>Hair Color: {i.hair_color}</p>
+        <p>Eyes: {i.eye_color}</p>
+        <p>Height in cm: {i.height}</p>
+      </div>
+    )
+  });
+return newList
+}
+
+export default Character
