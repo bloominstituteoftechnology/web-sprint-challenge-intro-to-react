58adeafb33f92906487959387c0adf10bcd2346c
Stuart Darsey <stuart.darsey@gmail.com>
2021-05-30 22:21:12 -0400

    mvp-met

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,31 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [charactersList, setCharactersList] = useState([])
+  const [charName, setCharName] = useState([])
+  const [charData, setCharData] = useState([])
+
+  const swapi = 'https://swapi.dev/api/people'
+
+  useEffect(() => {
+    axios
+      .get(swapi)
+      .then(response => {
+        // console.log(response.data)
+        setCharactersList(response.data)
+        setCharName(response.data.name)
+        setCharData(response.data)
+      })
+      .catch(error => {console.log(error)})
+      .finally(fin => {console.log('finally from axios call')})
+  }, [])
+
+
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character charactersList = {charactersList} />
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,52 @@
-// Write your Character component here
+import React, {useState} from 'react'
+import styled from 'styled-components'
+
+const StyledContainer = styled.div`
+width: 25vw;
+border: 5px solid green;
+margin: auto;
+`;
+
+const StyledDiv = styled.div`
+border: pink solid 5px;
+margin: 10px;
+padding: 3px;
+background-color: black;
+padding: 10px;
+`;
+
+const StyledName = styled.h2`
+color: white;
+font-size: 24px;
+text-transform: uppercase;
+background-color: dodgerblue;
+letter-spacing: 1px;
+`;
+
+const StyledData = styled.p`
+color: white;
+font-size: 18px;
+line-height: 2;
+`;
+
+export default function Character(props) {
+   const {charactersList, charData, charName} = props;
+
+   return (
+    <StyledContainer>
+        {charactersList.map((char) => {
+            return (
+                <StyledDiv>
+                   <StyledName>{char.name}</StyledName>
+                       <StyledData>
+                           <div>Height: {char.height}</div>
+                           <div>Hair Color: {char.hair_color}</div>
+                           <div>Birth Year:{char.birth_year}</div>
+                           <div>Weight: {char.mass}</div>
+                       </StyledData>
+               </StyledDiv>
+            )
+        })}; 
+    </StyledContainer>   
+   )
+}
