a4f58c337f508a31c47079d5b369183246d7be0b
scoutreilly <scouty33@gmail.com>
2021-05-31 00:33:48 -0600

    a little styling

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,24 +1,43 @@
 /* eslint-disable no-lone-blocks */
-// Write your Character component here
 import React from 'react';
+import styled from 'styled-components';
 
 function Character(props) {
   const characterDeets = props.props;
-  console.log(characterDeets);
+  // console.log(characterDeets);
   const newList = characterDeets.map(i => {
     return (
-      <div>
-        <p>My name is {i.name}</p>
-        <p>Check me out</p>
+      <MainDiv>
+        <HeaderFour>My name is {i.name}</HeaderFour>
+        <HeaderFive>Check out my stats</HeaderFive>
         <p>Gender: {i.gender}</p>
         <p>Birth: {i.birth_year}</p>
         <p>Hair Color: {i.hair_color}</p>
         <p>Eyes: {i.eye_color}</p>
         <p>Height in cm: {i.height}</p>
-      </div>
+      </MainDiv>
     )
   });
 return newList
 }
 
 export default Character
+
+const MainDiv = styled.div`
+  height: 100%;
+  text-content: center;
+  background-color: #e0dcda;
+  color: #2e1e18;
+  margin: 1% 40%;
+  padding: 10px;
+  border: 1px solid #b0a9a5;
+  border-radius: 16px;
+  margin-top: none!important;
+  margin-bottom: none!important;
+`;
+const HeaderFour = styled.h4`
+  text-shadow: 1px 1px 5px #fff;
+`;
+const HeaderFive = styled.h5`
+  text-shadow: 1px 1px 5px #fff;
+`;
