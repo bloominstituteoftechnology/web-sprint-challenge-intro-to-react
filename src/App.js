4b454fca8470999274fde30b5a44757ced51846f
Collin-Falstad <wildkard91@gmail.com>
2021-06-27 10:24:27 -0400

    built initial logic for character state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React from 'react';
 import './App.css';
+import axios from "axios"
+import React, { useState, useEffect } from "react";
+import Character from "./components/Character"
 
-const myName = "Collin"
+
 
 const App = () => {
+  const  [ character, setCharacter ] = useState([])
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(info => {
+      console.log(info)
+      setCharacter(info.data)
+    })
+
+  }, [])
+
+
+  
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star WAR</h1>
+      {characters.map((char, i) => {
+        return <Charater key={i} char={char} />;
+      })}
     </div>
   );
-}
+  }
 
 export default App;
