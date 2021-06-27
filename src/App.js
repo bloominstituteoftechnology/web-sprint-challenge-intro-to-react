ec26d5b3f97eddccd8994ca8a2c743d4f64b9c2f
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-06-27 15:23:10 -0500

    set up character, now to do the api request

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import Character from '../src/components/Character'
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -10,9 +12,7 @@ const App = () => {
   // sync up with, if any.
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <Character />
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,10 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react'
+
+const Character = () =>  {
+	return (
+		<div> hello from character </div>
+	)
+}
+
+export default Character
