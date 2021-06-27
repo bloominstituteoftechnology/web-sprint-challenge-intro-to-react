da7186168c1ce1bc4402a3d0465db044c7b12d4e
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-06-27 15:29:01 -0500

    useState and useEffect have been set up, reading through data to map through array

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,25 @@
 // Write your Character component here
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const Character = () =>  {
+
+
+const Character = () => {
+	const [character, setCharacter] = useState([])
+
+	useEffect(() => {
+		
+		axios
+		.get('https://swapi.dev/api/people')
+		.then(res => {
+			console.log(res)
+		})
+		.catch(err => {
+			console.log('something went wrong', err)
+		},[])
+	})
+
+
 	return (
 		<div> hello from character </div>
 	)
