d7844a1c278ea6880ccd3f27a8e904f5a8b9bddb
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-06-27 17:33:07 -0500

    figured out why i was failing, i did not need to create an extra component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Character from '../src/components/Character'
-
+import axios from 'axios'
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+ const [character, setCharacter] = useState([])
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+	useEffect(() => {
+		
+		axios
+		.get('https://swapi.dev/api/people')
+		.then(res => {
+			console.log(res)
+			setCharacter(res.data)
+		})
+		.catch(err => {
+			console.log('something went wrong', err)
+		})
+	},[])
+
 
   return (
-    <Character />
+    <div>
+			{character.map((char, key) => (<Character key={key} character={char} />))}
+		</div>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,32 +1,17 @@
 // Write your Character component here
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import CharacterCard from './CharacterCard'
+import React from 'react'
 
-
-
-const Character = () => {
-	const [character, setCharacter] = useState([])
-
-	useEffect(() => {
-		
-		axios
-		.get('https://swapi.dev/api/people')
-		.then(res => {
-			console.log(res)
-			setCharacter(res.data)
-		})
-		.catch(err => {
-			console.log('something went wrong', err)
-		})
-	},[])
-
+const Character = ({character}) => {
 
 	return (
-		<div>
-			{character.map((char, key) => (<CharacterCard key={key} character={char} />))}
+		<div>Name: {character.name}
+			<div>Height: {character.height}</div>
+			Movies: {character.films.map((movie, key) => { return (<div key={key}>{movie}</div>)})}
 		</div>
+
 	)
+
+	
 	
 }
 
