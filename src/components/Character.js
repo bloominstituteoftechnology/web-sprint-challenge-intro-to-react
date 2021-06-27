5a44bf2b6bd08cfea2756bf0275f42536d834eec
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-06-27 17:59:04 -0500

    i hate styling

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,13 +1,32 @@
 // Write your Character component here
 import React from 'react'
+import styled from 'styled-components'
 
-const Character = ({character}) => {
-
+const Character = ({ character }) => {
+	
+	const Card = styled.div
+		`
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		padding: 5px;
+		margin: 5px;
+		`
+	const CardTitle = styled.div
+		`
+		margin:
+		`	
 	return (
-		<div>Name: {character.name}
-			<div>Height: {character.height}</div>
-			Movies: {character.films.map((movie, key) => { return (<div key={key}>{movie}</div>)})}
-		</div>
+		<Card>
+			<CardTitle>
+				Name: {character.name}
+			</CardTitle>
+			<div>
+				Height: {character.height}</div>
+			<div>
+				Movies: {character.films.map((movie, key) => { return (<div key={key}>{movie}</div>) })}
+			</div>
+		</Card>
 
 	)
 
