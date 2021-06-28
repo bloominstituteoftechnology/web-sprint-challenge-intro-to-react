c2b8f75ef361e3e971a694d1bfba22b44c2d9e3e
jose-mercado <josefmercado99@gmail.com>
2021-06-27 22:55:56 -0700

    Finished Sprint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Characters from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +12,24 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([]);
+
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then((res) => {
+      console.log('It worked!!!! :)', res)
+      setCharacters(res.data);
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">React Star Wars!!!</h1>
+      <Characters data={characters}/>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,44 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components'
+
+
+const Characters = (props) => {
+
+    const { data } = props
+
+    return (
+        <Card>
+            {data.map((character) => {
+                return (
+                    <IndividualCard>
+                        <h1> Name:<br></br> {character.name}</h1>
+                        <h2>DOB:<br></br> {character.birth_year}</h2>
+                        <h2>Gender: {character.gender}</h2>
+                        <h2>Height: {character.height}</h2>
+                    </IndividualCard>
+                )
+            })}
+        </Card>
+    )
+}
+
+const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+
+`
+const IndividualCard = styled.div`
+    width: 30em;
+    border: 1px solid black;
+    margin: 2vh 2vh;
+    border-radius: 15px;
+    background-color: #C2B280;
+`
+
+
+
+export default Characters
