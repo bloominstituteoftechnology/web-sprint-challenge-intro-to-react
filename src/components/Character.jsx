3ed634344ce036f026ebd3a84be11350e19673c7
edurazo12 <74686150+edurazo12@users.noreply.github.com>
2021-07-08 20:38:01 -0700

    update

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,28 +1,53 @@
 import React from 'react'
-import Character from './Character'
-import styled from 'styled-components'
-
-const CharactersList = (props) => {
-    console.log(props)
-
-    const Section = styled.section `
-        display: flex;
-        flex-wrap: wrap;
-        `
-
-    return(
-        <section>
-            {
-                props.characters.map((character, index) => (
-                   <Characeter key ={`Character-${character.name}-${index}`}
-                   character ={character}/>
-
-                ))
-            }
-        </section>
-    )
+import styled from 'styled-components';
 
 
+function Character(props) {
+    const characterDeets = props.props;
+    console.log(characterDeets);
+    
+    const newList = characterDeets.map(i => {
+        return (
+            <MainDiv>
+            
+                <HeaderFour>My name is {i.name} </HeaderFour>
+                <HeaderFive>Check out my stats</HeaderFive>
+                <p>Gender: {i.gender}</p>
+                <p>Birth: {i.birth_year}
+                <p>Eyes: {i.eye.color}</p>
+                <p>Height in cm: {i.height}</p>
+            </MainDiv>
+        )
+});
+return newList 
 }
 
-export default CharactersList
+export default Character
+
+const MaiDiv = styled.div`
+  height: 100%;
+  text-content: center;
+  background-color: #e0dcda;
+  color: #2e1e18;
+  margin: 1% 40%;
+  padding: 10px;
+  border: 1px solid #b0a9a5;
+  margin-top: none!important;
+  margin-bottom: none!important;
+  
+  ;
+  const HeaderFour = styled.h4`
+  text-shadow: 1px 1px 5px #fff;d
+  
+ `;
+ 
+ const HeaderFive = styled.h5`
+  text-shadow: 1px 1px 5px #fff;
+`;
+  
+  
+  
+  
+  
+  
+                                 
