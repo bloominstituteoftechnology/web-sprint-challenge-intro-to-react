fb28cdf6289fbb0efee4bf7b2ef09069bdf64d70
unknown <widespread.space@gmail.com>
2021-07-16 18:30:43 -0400

    finished project, now working on mvp

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+
+const Card = () => {
+    return (
+      <div className="card">
+          <p>This is a 'Card'</p>
+      </div>
+    );
+  }
+  
+  export default Card;
+  
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+
+const Header = () => {
+    return (
+      <div className="page-header">
+          <h3>Page Header</h3>
+      </div>
+    );
+  }
+  
+  export default Header;
+  
