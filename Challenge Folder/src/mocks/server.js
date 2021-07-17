b89dc99bf2a5ba6ef211c673ab76f29404bf1f4c
Thomas Bielawski <tfbielawski@protonmail.com>
2021-07-16 20:20:39 -0400

    submitting again. Git folder size error

--- Challenge Folder/src/mocks/server.js
+++
@@ -1,7 +0,0 @@
-// DO NOT MAKE CHANGES TO THIS FILE
-// DO NOT MAKE CHANGES TO THIS FILE
-// DO NOT MAKE CHANGES TO THIS FILE
-import { setupServer } from "msw/node";
-import { handlers } from "./handlers";
-
-export const server = setupServer(...handlers);
