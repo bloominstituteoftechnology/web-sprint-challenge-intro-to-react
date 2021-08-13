42c7c466ef5dc23b751f7e8be93f2b9b45f46ed4
Anastasia Lapteva <anastasia_laptev@hotmail.com>
2021-08-13 13:07:36 -0700

    added homeworld

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ Your finished project must include all of the following requirements:
 
 - [X] Use the endpoint `[GET] https://swapi.dev/api/people` (mocked in [msw](https://github.com/mswjs/msw)) to obtain characters.
 - [X] Set the list of characters into state.
-- [ ] Render your characters to the DOM:
+- [X] Render your characters to the DOM:
 
   1. Build a React component named 'Character' to render an individual character.
   2. Map over the list in state, and for each character render a Character to the page.
@@ -81,6 +81,6 @@ After finishing your required elements, you can push your work further. These go
 Be prepared to demonstrate your understanding of this week's concepts by answering questions on the following topics. You might prepare by writing down your own answers before hand.
 
 1. What is React JS and what problems does it solve? Support your answer with concepts introduced in class and from your personal research on the web.
-1. Describe component state.
-1. Describe props.
-1. What are side effects, and how do you sync effects in a React component to changes of certain state or props?
+2. Describe component state.
+3. Describe props.
+4. What are side effects, and how do you sync effects in a React component to changes of certain state or props?
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 
 export default function Details(props)
 {
+    const [homeworld, setHomeworld] = useState(null);
+
+    useEffect(() =>
+    {
+        console.log(props.info[0].homeworld);
+        console.log("Fetching the homeworld...");
+        axios.get(`${props.info[0].homeworld}`)
+            .then(response =>
+            {
+                console.log(response.data);
+                setHomeworld(response.data);
+            })
+            .catch(err =>
+            {
+                console.error(err);
+            });
+    }, []);
+
     const Container = styled.div` 
         width: 70%;
         display: flex;
@@ -29,12 +48,27 @@ export default function Details(props)
         padding: 0.25em 1em;
     `;
 
+    const HomeworldContainer = styled.div`
+        padding: 8px;
+        background-color: rgba(127, 85, 57,.3);
+        color: #0077b6;
+    `;
+
     return (
         <Container>
             <h2>Details of {props.info[0].name}</h2>
             <p>Born in year: {props.info[0].birth_year}</p>
             <p>Eye color: {props.info[0].eye_color}</p>
             <p>Hair color: {props.info[0].hair_color}</p>
+            {
+                homeworld && <HomeworldContainer>
+                    <h3>Homeworld:</h3>
+                    <p>Name: {homeworld.name}</p>
+                    <p>Climate: {homeworld.climate}</p>
+                    <p>Terrain: {homeworld.terrain}</p>
+                    <p>Population: {homeworld.population}</p>
+                </HomeworldContainer>
+            }
 
             <Button onClick={props.closeDetails}>Close Details</Button>
         </Container>
