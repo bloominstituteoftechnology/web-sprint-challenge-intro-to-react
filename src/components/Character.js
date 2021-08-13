d62731e10757e0fe059bbb04a9b305ef5353942c
KRQuinn <kr.quinn82@gmail.com>
2021-08-13 14:20:23 -0700

    Working on character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,3 +3,23 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import styled from 'styled-components'
+
+export default function Character(char) {
+    
+
+    const [character, setCharacter] = useState([]);
+    
+    useEffect(() => {
+        axios.get('https://swapi.dev/api/people')
+          .then(res => {
+            setCharacter(res.data)
+          })
+          .catch(err => console.log(err))
+    }, []);
+
+    return (
+        <Character>
+            
+        </Character>
+    );
+}
