125467b299e58153c537dc8c54346230deae7eba
Jacob Brooks <myrefugemyfortress@yahoo.com>
2021-08-29 19:04:31 -0400

    Set up NPM tests and components

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,25 @@
-// Write your Character component here
+import React from 'react';
+
+
+
+
+const charApp = (props) => {
+  
+    return(
+      <section>
+           {
+            props.character.map((character) => (
+               <article>
+               <p>Name: {character.name}  Birth Year:{character.birth_year}</p>
+               </article>
+               
+               ))
+
+               
+           }
+      </section>   
+    );
+  }
+
+
+export default charApp;
