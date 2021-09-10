d76d1812b666f0e105d8daf6dc215eb6a972f292
David Fletcher <davidfletcher@Davids-Air-2.lan>
2021-09-10 09:31:25 -0700

    initial commit

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,10 @@
 // Write your Character component here
+import React, {useState, useEffect}  from 'react';
+
+export default function Character (props){
+    const { name, age, something} = props
+
+    return(
+        <Character nameObj={name} ageObj={age}/>
+    )
+}
