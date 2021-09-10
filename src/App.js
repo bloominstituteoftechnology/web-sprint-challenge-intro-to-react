1ca29ef4424a9984d8c15cfa60ea79b583e99d65
user <useruser@Guests-MacBook-Pro.local>
2021-09-10 12:01:53 -0400

    Test passing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+import Character from './components/Character'
 
 const App = () => {
+  const [starwarsCharacter, setstarwarsCharacter] = useState([])
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then(res => {
+        setstarwarsCharacter(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  console.table(starwarsCharacter)
+  const Character = starwarsCharacter.map(inner => inner.name + inner.height + inner.mass + inner.hair_color + inner.skin_color)
+
+  console.log(Character)
+
+  return (
+    <div className="App">
+      <h1 className="Header">Characters</h1>
+      {Character}
+
+
+    </div>
+  );
+}
+
+export default App;
+
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,11 +41,3 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
-}
-
-export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,19 @@
+
 // Write your Character component here
+import React from 'react'
+
+
+// function Character(props) {
+//     const { starwarsCharacter } = props
+// }
+
+
+// return (
+//     <div classname='character'>
+//         <h2>Name:{starwarsCharacter.name}</h2>
+//         <p>height:{starwarsCharacter.height}</p>
+//         <p>mass:{starwarsCharacter.mass}</p>
+//         <p>hair_color:{starwarsCharacter.hair_color}</p>
+//         <p>skin_color:{starwarsCharacter.skin_color}</p>
+//     </div>
+// 
