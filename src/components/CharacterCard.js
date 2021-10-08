fb238cf9dc998326267137f61a5cd52d50fc9ca7
Kseniya Platonava <43353550+Kseniyapl@users.noreply.github.com>
2021-10-08 13:36:39 -0400

    added card information

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,7 +3,15 @@ import React from 'react'
  const CharacterCard = ({item}) => {
      return (
     <div className='card'>
-{item.name}
+<h3>{item.name}</h3>
+<div> Gender: {item.gender}</div>
+<div> Height: {item.height}</div>
+<div> Mass: {item.mass}</div>
+<div> Birth Year: {item.birth_year}</div>
+<div> Eye Color: {item.eye_color}</div>
+<div> Hair Color: {item.hair_color}</div>
+<div> Mass: {item.mass}</div>
+<div> Skin Color: {item.skin_color}</div>
      </div>
  )}
     
