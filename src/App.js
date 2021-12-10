3be89a9662cab5c3975de567d5174697809dcabd
abdullahi0520 <90243393+abdullahi0520@users.noreply.github.com>
2021-12-10 07:39:24 -0600

    passing tests

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+import styled from 'styled-components';
 
 const App = () => {
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [starFolk,setStarFolk] = useState(null);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(res => {
+      setStarFolk(res.data.results);
+      setStarFolk(res.data);
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  }, []);
 
+
+  const StarDiv = styled.div`
+  margin: 0 auto;
+  box-sizing: border-box;
+  width: 50%;
+`
+ 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+    <h1 className="Header">Characters</h1>
+    <StarDiv>
+    {
+      starFolk &&
+      <>
+      {
+        starFolk.map((character, index) => {
+          return <Character key={index} info={character}/>
+        })
+      }
+      </>
+    }
+    </StarDiv>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,43 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const StarStyle = styled.div`
+display:flex;
+justify-content:center;
+`
+
+const StarStyleBox = styled.div`
+margin:10% 15% 0;
+background-color:coral;
+border: solid black 5px;
+border-radius:10%
+`
+    
+const Character = props => {
+    const {info} =props
+    return (
+        <StarStyle>
+            <h1>{info.name}</h1>
+        </StarStyle>
+    )
+}
+
+
+    // return (
+    //     <starStyle>
+    //     {starData.map(e => {
+    //         return (
+    //             <starStyleBox>
+    //             <h1>{e.name}</h1>
+    //             <p>{e.name} was born on {e.birth_year}.</p>
+    //             <p>{e.name} is {e.height}cm tall, and is {e.mass}kg. </p>
+    //             <p>They were in the movie: {e.films.map(e => <div>{e}</div>)}</p>
+    //             </starStyleBox>
+    //         )   
+    //     })};
+    
+    //     </starStyle >
+    // )
+
+export default Character;
