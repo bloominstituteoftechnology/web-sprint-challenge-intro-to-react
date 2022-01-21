c7a1d95eb508077f80792932076fca31d9599eda
Mike Lovell <mike@Carlys-MacBook-Air.local>
2022-01-21 11:24:38 -0800

    began

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 const App = () => {
@@ -8,12 +8,88 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+const [character, setCharacter] = useState([
+  
+    {
+      name: "Luke Skywalker", 
+      height: "172", 
+      mass: "77", 
+      hair_color: "blond", 
+      skin_color: "fair", 
+      eye_color: "blue", 
+      birth_year: "19BBY", 
+      gender: "male", 
+      homeworld: "https://swapi.dev/api/planets/1/", 
+      films: [
+          "https://swapi.dev/api/films/1/", 
+          "https://swapi.dev/api/films/2/", 
+          "https://swapi.dev/api/films/3/", 
+          "https://swapi.dev/api/films/6/"
+      ], 
+      species: [], 
+      vehicles: [
+          "https://swapi.dev/api/vehicles/14/", 
+          "https://swapi.dev/api/vehicles/30/"
+      ], 
+      starships: [
+          "https://swapi.dev/api/starships/12/", 
+          "https://swapi.dev/api/starships/22/"
+      ], 
+      created: "2014-12-09T13:50:51.644000Z", 
+      edited: "2014-12-20T21:17:56.891000Z", 
+      url: "https://swapi.dev/api/people/1/"
+  },
+  {
+    name: "Luke Skywalker", 
+    height: "172", 
+    mass: "77", 
+    hair_color: "blond", 
+    skin_color: "fair", 
+    eye_color: "blue", 
+    birth_year: "19BBY", 
+    gender: "male", 
+    homeworld: "https://swapi.dev/api/planets/1/", 
+    films: [
+        "https://swapi.dev/api/films/1/", 
+        "https://swapi.dev/api/films/2/", 
+        "https://swapi.dev/api/films/3/", 
+        "https://swapi.dev/api/films/6/"
+    ], 
+    species: [], 
+    vehicles: [
+        "https://swapi.dev/api/vehicles/14/", 
+        "https://swapi.dev/api/vehicles/30/"
+    ], 
+    starships: [
+        "https://swapi.dev/api/starships/12/", 
+        "https://swapi.dev/api/starships/22/"
+    ], 
+    created: "2014-12-09T13:50:51.644000Z", 
+    edited: "2014-12-20T21:17:56.891000Z", 
+    url: "https://swapi.dev/api/people/1/"
+}
+  
+
+  
+])
+
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      {
+        character.map(character => 
+         <article>
+      <h1 className="Header">{character.name}</h1>
+      </article>)
+        
+      }
     </div>
-  );
-}
+        );
+  }
+   
+      
+  
+      
 
 export default App;
+      
--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+// import Character from '../src/components';
+
 import { worker } from "./mocks/browser";
 worker.start();
 
 ReactDOM.render(<App />, document.getElementById("root"));
+ 
