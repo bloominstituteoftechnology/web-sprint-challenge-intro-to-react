1f0cf06728c82bb01a4f497adeb77407146c32fc
Mike Lovell <mike@Carlys-MacBook-Air.local>
2022-02-18 09:56:19 -0800

    added axios, usestate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React,{ useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+
 
 const App = () => {
+  const [characters, setCharacter] = useState([])
+  const [currentCharacterId, setCurrentCharacterId] = useState(null)
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,6 @@
+import React from "react";
+
+
+
+
 // Write your Character component here
