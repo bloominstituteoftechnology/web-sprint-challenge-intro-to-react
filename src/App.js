c9317ccd2cb44621fddf2be8e3a127c43d84fd5d
Mike Lovell <mike@Carlys-MacBook-Air.local>
2022-02-18 11:34:23 -0800

    added map function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React,{ useState} from 'react';
+import React,{ useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 import Character from './components/Character';
-
+// import characterData from 'https://swapi.dev/api/films';
 
 const App = () => {
-  const [characters, setCharacter] = useState([])
+  const [characters, setCharacter] = useState([
+  {
+  name: "Luke Skywalker",
+  id:0,
+  height: 172, 
+  mass: 77, 
+  hair_color: "blond", 
+  skin_color: "fair", 
+  eye_color: "blue", 
+  birth_year: "19BBY", 
+  gender: "male", 
+},
+{
+  name: "Luke Skywalkerrr",
+  id:1,
+  height: 172, 
+  mass: 77, 
+  birth_year: "19BBY", 
+  gender: "male", 
+}
+
+])
+
+console.log(characters)
   const [currentCharacterId, setCurrentCharacterId] = useState(null)
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -16,9 +39,21 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">
+        {
+        characters.map(character => <article>
+        <h2>{character.name}</h2>
+        <p>{character.id}</p>
+        <p>{character.height}</p>
+        <p>{character.mass}</p>
+        <p>{character.birth_year}</p>  
+        <p>{character.gender}</p>  
+          </article>
+         )
+        }
+      </h1>
     </div>
-  );
+          );
 }
 
 export default App;
