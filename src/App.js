e68c8dda7f1689909c1f53ca8d67429d8cb74b6f
Casey Harding <casey.r.harding@gmail.com>
2022-03-03 14:44:35 -0600

    Moved global style imports to index.js to account for broken jest.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import "./App.css";
 
 import { worker } from "./mocks/browser";
 worker.start();
