d5482263577ac3e79b1bf315ed492627d8420860
CRHarding <34920615+CRHarding@users.noreply.github.com>
2022-06-16 13:32:15 -0400

    Update handlers.js

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -5,6 +5,9 @@ export const handlers = [
   rest.get("https://swapi.dev/api/people/", (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(data));
   }),
+  rest.get("https://swapi.dev/api/films/", (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(films));
+  }),
 ];
 
 export const data = [
