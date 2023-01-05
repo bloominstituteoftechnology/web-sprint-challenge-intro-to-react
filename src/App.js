492656f093572c6b6e7acd4c6dd3bfb9c64211e3
Aodhan Roche <aodhanroche@Aodhans-MacBook-Pro.local>
2023-01-05 10:26:33 -0800

    Checkpoint.1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+//import Character from './components/Character';
 
-const App = () => {
+function App() {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -8,11 +10,31 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([]);
+
+  const [featuredCharacter, setFeaturedCharacter] = useState();
+
+  //const [featuredCharacter, setFeaturedCharacter] = useState();
+
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+      .then(res => {
+        setCharacters(res.data.results);
+      }).catch(err => console.error(err));
+  }, []);
+
+
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div>
+      <h1 className='Header'>Star Wars Characters</h1>
+      { 
+        characters.map(dev => 
+          <p>{dev.name} <button onClick={() => {setFeaturedCharacter(dev.id)}}>More Info</button></p>
+        )
+      }
     </div>
   );
-}
 
-export default App;
+}
+export default App; 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,12 @@
 // Write your Character component here
+import React from "react";
+
+const Character = props => {
+    return (
+        <div className='Header'>
+            <h2 className='Name'>Name: {props.character.name}</h2>
+        </div>
+    )
+}
+
+export default Character;
