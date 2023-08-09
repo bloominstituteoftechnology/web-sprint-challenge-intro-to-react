bbe47488b6884e8042f2cb101195d5153cae09a7
Ibrahim <Ibrahimagbor1@gmail.com>
2023-08-09 14:00:36 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from 'react';
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Character from './components/Character'
 const App = () => {
+
+  const [characters,setCharacters] = useState([])
+  useEffect(() => {
+axios.get(`https://swapi.dev/api/people/`)
+.then(res=> {
+  console.log(res.data)
+setCharacters(res.data)
+})
+.catch(err => console.log(err))
+  }, [])
+ 
+
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -9,8 +24,13 @@ const App = () => {
   // sync up with, if any.
 
   return (
+   
     <div className="App">
+    
       <h1 className="Header">Characters</h1>
+      {characters.map(char => {
+        return <Character name={char.name} />
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+import React from 'react'
+
+function Character(props) {
+  return (  
+<div>
+  <h2>{props.name}</h2>
+  <button> Click to know more </button>
+</div>
+  )
+}
+
+export default Character
